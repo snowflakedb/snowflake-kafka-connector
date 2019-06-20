87041ff188a33c597d2949b2352b294c07f61f73
Bing Li <binglihub@gmail.com>
2019-06-20 15:44:04 -0700

    upgrade snowflake jdbc to 3.8.4

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -38,7 +38,7 @@ public class Utils
 {
 
   //Connector version, change every release
-  public static final String VERSION = "0.3";
+  public static final String VERSION = "0.3.1";
 
   //Connector parameters
   public static final long MAX_RECOVERY_TIME = 10 * 24 * 3600 * 1000; //10 days
