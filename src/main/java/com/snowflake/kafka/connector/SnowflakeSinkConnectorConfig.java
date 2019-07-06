3f9704cb1734aac7e616d3079f6cff02a7a93926
Bing Li <binglihub@gmail.com>
2019-07-06 08:31:04 -0700

    add new schema registry authentication parameters

--- src/main/java/com/snowflake/kafka/connector/SnowflakeSinkConnectorConfig.java
+++ src/main/java/com/snowflake/kafka/connector/SnowflakeSinkConnectorConfig.java
@@ -19,7 +19,6 @@ package com.snowflake.kafka.connector;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.common.config.ConfigDef.Type;
 import org.apache.kafka.common.config.ConfigDef.Importance;
-
 /**
  * SnowflakeSinkConnectorConfig class is used for specifying the set of
  * expected configurations.
@@ -29,117 +28,153 @@ import org.apache.kafka.common.config.ConfigDef.Importance;
  * configuration value,
  * and the name suitable for display in the UI.
  */
-public class SnowflakeSinkConnectorConfig
+class SnowflakeSinkConnectorConfig
 {
 
   static final String NAME = "name";
   static final String TOPICS = "topics";
 
-  // snowflake kafka connector config
+  // Connector config
+  private static final String CONNECTOR_CONFIG = "Connector Config";
   static final String BUFFER_COUNT_RECORDS = "buffer.count.records";
-  static final long BUFFER_COUNT_RECORDS_DEFAULT = 10000;
-
   static final String BUFFER_SIZE_BYTES = "buffer.size.bytes";
-  static final long BUFFER_SIZE_BYTES_DEFAULT = 5000000;
+  static final String TOPICS_TABLES_MAP = "snowflake.topic2table.map";
+
 
   // snowflake connection and database config
-
+  private static final String SNOWFLAKE_LOGIN_INFO = "Snowflake Login Info";
   static final String SNOWFLAKE_URL = Utils.SF_URL;
-  static final String SNOWFLAKE_URL_DEFAULT = "";
-
   static final String SNOWFLAKE_USER = Utils.SF_USER;
-  static final String SNOWFLAKE_USER_DEFAULT = "";
-
   static final String SNOWFLAKE_PRIVATE_KEY = Utils.SF_PRIVATE_KEY;
-  static final String SNOWFLAKE_PRIVATE_KEY_DEFAULT = "";
-
   static final String SNOWFLAKE_DATABASE = Utils.SF_DATABASE;
-  static final String SNOWFLAKE_DATABASE_DEFAULT = "";
-
   static final String SNOWFLAKE_SCHEMA = Utils.SF_SCHEMA;
-  static final String SNOWFLAKE_SCHEMA_DEFAULT = "";
 
-  static final String TOPICS_TABLES_MAP = "snowflake.topic2table.map";
-  static final String TOPICS_TABLES_MAP_DEFAULT = "";
-
-  static final String REGISTRY_URL = "value.converter.schema.registry.url";
-  static final String REGISTRY_URL_DEFAULT = "";
-
-  //jvm proxy
+  //Proxy Info
+  private static final String PROXY_INFO = "Proxy Info";
   static final String JVM_PROXY_HOST = "jvm.proxy.host";
-  static final String JVM_PROXY_HOST_DEFAULT = "";
-  
   static final String JVM_PROXY_PORT = "jvm.proxy.port";
-  static final String JVM_PROXY_PORT_DEFAULT = "";
-  
+
+  //Schema Registry Info
+  private static final String SCHEMA_REGISTRY_INFO = "Schema Registry Info";
+  static final String SCHEMA_REGISTRY_AUTH_CREDENTIALS_SOURCE =
+    "value.converter.basic.auth.credentials.source";
+  static final String SCHEMA_REGISTRY_AUTH_USER_INFO =
+    "value.converter.basic.auth.user.info";
+  static final String REGISTRY_URL = "value.converter.schema.registry.url";
 
   static ConfigDef newConfigDef()
   {
     return new ConfigDef()
-        // snowflake avro converter config
-        .define(REGISTRY_URL,
-            Type.STRING,
-            REGISTRY_URL_DEFAULT,
-            Importance.LOW,
-            "Required by SnowflakeAvroConnector if schema registry is used. " +
-                "Leave blank if schema is included in AVRO record")
-        // snowflake kafka connector config
-        .define(BUFFER_COUNT_RECORDS,
-            Type.LONG,
-            BUFFER_COUNT_RECORDS_DEFAULT,
-            Importance.LOW,
-            "Number of records buffered in memory per partition before " +
-                "triggering Snowflake ingestion")
-        .define(BUFFER_SIZE_BYTES,
-            Type.LONG,
-            BUFFER_SIZE_BYTES_DEFAULT,
-            Importance.LOW,
-            "Cumulative size of records buffered in memory per partition " +
-                "before triggering Snowflake ingestion")
-        // snowflake connection and database config
-        .define(SNOWFLAKE_URL,
-            Type.STRING,
-            SNOWFLAKE_URL_DEFAULT,
-            Importance.HIGH,
-            "Snowflake account URL")
-        .define(SNOWFLAKE_USER,
-            Type.STRING,
-            SNOWFLAKE_USER_DEFAULT,
-            Importance.HIGH,
-            "Snowflake user name")
-        .define(SNOWFLAKE_PRIVATE_KEY,
-            Type.STRING,
-            SNOWFLAKE_PRIVATE_KEY_DEFAULT,
-            Importance.HIGH,
-            "Private key for Snowflake user")
-        .define(SNOWFLAKE_DATABASE,
-            Type.STRING,
-            SNOWFLAKE_DATABASE_DEFAULT,
-            Importance.HIGH,
-            "Snowflake database name")
-        .define(SNOWFLAKE_SCHEMA,
-            Type.STRING,
-            SNOWFLAKE_SCHEMA_DEFAULT,
-            Importance.HIGH,
-            "Snowflake database schema name")
-        .define(TOPICS_TABLES_MAP,
-            Type.STRING,
-            TOPICS_TABLES_MAP_DEFAULT,
-            Importance.MEDIUM,
-            "Map of topics to tables (optional)." +
-                " Format : comma-seperated tuples, e.g. <topic-1>:<table-1>," +
-                "<topic-2>:<table-2>,... ")
-        .define(JVM_PROXY_HOST,
-            Type.STRING,
-            JVM_PROXY_HOST_DEFAULT,
-            Importance.LOW,
-            "jvm option: https.proxyHost")
-        .define(JVM_PROXY_PORT,
-            Type.STRING,
-            JVM_PROXY_PORT_DEFAULT,
-            Importance.LOW,
-            "jvm option: https.proxyPort")
-        ;
+      //snowflake login info
+      .define(SNOWFLAKE_URL,
+        Type.STRING,
+        Importance.HIGH,
+        "Snowflake account url",
+        SNOWFLAKE_LOGIN_INFO,
+        0,
+        ConfigDef.Width.NONE,
+        SNOWFLAKE_URL)
+      .define(SNOWFLAKE_USER,
+        Type.STRING,
+        Importance.HIGH,
+        "Snowflake user name",
+        SNOWFLAKE_LOGIN_INFO,
+        1,
+        ConfigDef.Width.NONE,
+        SNOWFLAKE_USER)
+      .define(SNOWFLAKE_PRIVATE_KEY,
+        Type.STRING,
+        Importance.HIGH,
+        "Private key for Snowflake user",
+        SNOWFLAKE_LOGIN_INFO,
+        2,
+        ConfigDef.Width.NONE,
+        SNOWFLAKE_PRIVATE_KEY)
+      .define(SNOWFLAKE_DATABASE,
+        Type.STRING,
+        Importance.HIGH,
+        "Snowflake database name",
+        SNOWFLAKE_LOGIN_INFO,
+        3,
+        ConfigDef.Width.NONE,
+        SNOWFLAKE_DATABASE)
+      .define(SNOWFLAKE_SCHEMA,
+        Type.STRING,
+        Importance.HIGH,
+        "Snowflake database schema name",
+        SNOWFLAKE_LOGIN_INFO,
+        4,
+        ConfigDef.Width.NONE,
+        SNOWFLAKE_SCHEMA)
+      //proxy
+      .define(JVM_PROXY_HOST,
+        Type.STRING,
+        Importance.LOW,
+        "JVM option: https.proxyHost",
+        PROXY_INFO,
+        0,
+        ConfigDef.Width.NONE,
+        JVM_PROXY_HOST
+        )
+      .define(JVM_PROXY_PORT,
+        Type.STRING,
+        Importance.LOW,
+        "JVM option: https.proxyPort",
+        PROXY_INFO,
+        1,
+        ConfigDef.Width.NONE,
+        JVM_PROXY_PORT)
+      //schema registry
+      .define(REGISTRY_URL,
+        Type.STRING,
+        Importance.LOW,
+        "Required by SnowflakeAvroConnector if schema registry is used. Leave blank if schema is included in AVRO record",
+        SCHEMA_REGISTRY_INFO,
+        0,
+        ConfigDef.Width.NONE,
+        REGISTRY_URL)
+      .define(SCHEMA_REGISTRY_AUTH_CREDENTIALS_SOURCE,
+        Type.STRING,
+        Importance.LOW,
+        "Required by SnowflakeAvroConnector if schema registry authentication used. e.g USER_INFO",
+        SCHEMA_REGISTRY_INFO,
+        1,
+        ConfigDef.Width.NONE,
+        SCHEMA_REGISTRY_AUTH_CREDENTIALS_SOURCE)
+      .define(SCHEMA_REGISTRY_AUTH_USER_INFO,
+        Type.STRING,
+        Importance.LOW,
+        "User info of schema registry authentication, format: <user name>:<password>",
+        SCHEMA_REGISTRY_INFO,
+        2,
+        ConfigDef.Width.NONE,
+        SCHEMA_REGISTRY_AUTH_USER_INFO
+        )
+      //Connector Config
+      .define(TOPICS_TABLES_MAP,
+        Type.STRING,
+        Importance.LOW,
+        "Map of topics to tables (optional). Format : comma-seperated tuples, e.g. <topic-1>:<table-1>,<topic-2>:<table-2>,... ",
+        CONNECTOR_CONFIG,
+        0,
+        ConfigDef.Width.NONE,
+        TOPICS_TABLES_MAP)
+      .define(BUFFER_COUNT_RECORDS,
+        Type.LONG,
+        Importance.LOW,
+        "Number of records buffered in memory per partition before triggering Snowflake ingestion",
+        CONNECTOR_CONFIG,
+        1,
+        ConfigDef.Width.NONE,
+        BUFFER_COUNT_RECORDS)
+      .define(BUFFER_SIZE_BYTES,
+        Type.LONG,
+        Importance.LOW,
+        "Cumulative size of records buffered in memory per partition before triggering Snowflake ingestion",
+        CONNECTOR_CONFIG,
+        2,
+        ConfigDef.Width.NONE,
+        BUFFER_SIZE_BYTES)
+      ;
   }
-
 }
