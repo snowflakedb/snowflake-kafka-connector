5d84a56ecc670784a7ac33151893cc5030ab3396
Bing Li <binglihub@gmail.com>
2019-10-21 19:40:36 -0700

    change version number to 0.5.2

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -37,7 +37,7 @@ public class Utils
 {
 
   //Connector version, change every release
-  public static final String VERSION = "0.5.1";
+  public static final String VERSION = "0.5.2";
 
   //connector parameter list
   public static final String NAME = "name";
