97cb7fea2b1d0ae5c4cab2e1e57b893b1a388070
Bing Li <binglihub@gmail.com>
2019-10-28 17:10:59 -0700

    fix bc fips issue

--- src/main/java/com/snowflake/kafka/connector/internal/InternalUtils.java
+++ src/main/java/com/snowflake/kafka/connector/internal/InternalUtils.java
@@ -1,22 +1,14 @@
 package com.snowflake.kafka.connector.internal;
 
 import com.snowflake.kafka.connector.Utils;
+import net.snowflake.client.jdbc.internal.apache.commons.codec.binary.Base64;
+import net.snowflake.client.jdbc.internal.org.bouncycastle.jce.provider.BouncyCastleProvider;
 import net.snowflake.ingest.connection.IngestStatus;
-import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
-import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
-import org.bouncycastle.openssl.PEMKeyPair;
-import org.bouncycastle.openssl.PEMParser;
-import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
-import org.bouncycastle.openssl.jcajce.JceOpenSSLPKCS8DecryptorProviderBuilder;
-import org.bouncycastle.operator.InputDecryptorProvider;
-import org.bouncycastle.pkcs.PKCS8EncryptedPrivateKeyInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.StringReader;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
-import java.security.Security;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -82,70 +74,19 @@ class InternalUtils
     }
   }
 
-  static PrivateKey parseEncryptedPrivateKey(String key, String passphrase)
-  {
-    //remove header, footer, and line breaks
-    key = key.replaceAll("-+[A-Za-z ]+-+", "");
-    key = key.replaceAll("\\s", "");
-
-    StringBuilder builder = new StringBuilder();
-    builder.append("-----BEGIN ENCRYPTED PRIVATE KEY-----");
-    for (int i = 0; i < key.length(); i++)
-    {
-      if (i % 64 == 0)
-      {
-        builder.append("\n");
-      }
-      builder.append(key.charAt(i));
-    }
-    builder.append("\n-----END ENCRYPTED PRIVATE KEY-----");
-    key = builder.toString();
-    Security.addProvider(new BouncyCastleFipsProvider());
-    try
-    {
-      PEMParser pemParser = new PEMParser(new StringReader(key));
-      PKCS8EncryptedPrivateKeyInfo encryptedPrivateKeyInfo =
-        (PKCS8EncryptedPrivateKeyInfo) pemParser.readObject();
-      pemParser.close();
-      InputDecryptorProvider pkcs8Prov =
-        new JceOpenSSLPKCS8DecryptorProviderBuilder().build(passphrase.toCharArray());
-      JcaPEMKeyConverter converter =
-        new JcaPEMKeyConverter().setProvider(BouncyCastleFipsProvider.PROVIDER_NAME);
-      PrivateKeyInfo decryptedPrivateKeyInfo =
-        encryptedPrivateKeyInfo.decryptPrivateKeyInfo(pkcs8Prov);
-      return converter.getPrivateKey(decryptedPrivateKeyInfo);
-    } catch (Exception e)
-    {
-      throw SnowflakeErrors.ERROR_0018.getException(e);
-    }
-  }
-
   static PrivateKey parsePrivateKey(String key)
   {
     //remove header, footer, and line breaks
     key = key.replaceAll("-+[A-Za-z ]+-+", "");
     key = key.replaceAll("\\s", "");
 
-    StringBuilder builder = new StringBuilder();
-    builder.append("-----BEGIN RSA PRIVATE KEY-----");
-    for (int i = 0; i < key.length(); i++)
-    {
-      if (i % 64 == 0)
-      {
-        builder.append("\n");
-      }
-      builder.append(key.charAt(i));
-    }
-    builder.append("\n-----END RSA PRIVATE KEY-----");
-    key = builder.toString();
+    java.security.Security.addProvider(new BouncyCastleProvider());
+    byte[] encoded = Base64.decodeBase64(key);
     try
     {
-      PEMParser pemParser = new PEMParser(new StringReader(key));
-      PEMKeyPair pemKeyPair = (PEMKeyPair) pemParser.readObject();
-      PKCS8EncodedKeySpec keySpec =
-        new PKCS8EncodedKeySpec(pemKeyPair.getPrivateKeyInfo().getEncoded());
-      KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-      return keyFactory.generatePrivate(keySpec);
+      KeyFactory kf = KeyFactory.getInstance("RSA");
+      PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
+      return kf.generatePrivate(keySpec);
     } catch (Exception e)
     {
       throw SnowflakeErrors.ERROR_0002.getException(e);
@@ -217,8 +158,9 @@ class InternalUtils
 
     if (!privateKeyPassphrase.isEmpty())
     {
-      properties.put(JDBC_PRIVATE_KEY, parseEncryptedPrivateKey(privateKey,
-        privateKeyPassphrase));
+      properties.put(JDBC_PRIVATE_KEY,
+        EncryptionUtils.parseEncryptedPrivateKey(privateKey,
+          privateKeyPassphrase));
     }
     else if (!privateKey.isEmpty())
     {
