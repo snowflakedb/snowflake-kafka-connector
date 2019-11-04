60c52ad31db1c7697f2f9c4e03308134470a91ef
Bing Li <binglihub@gmail.com>
2019-11-04 10:29:44 -0800

    fix null pointer exception when shut down Sink Service

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -37,7 +37,7 @@ public class Utils
 {
 
   //Connector version, change every release
-  public static final String VERSION = "0.5.4";
+  public static final String VERSION = "0.5.5";
 
   //connector parameter list
   public static final String NAME = "name";
