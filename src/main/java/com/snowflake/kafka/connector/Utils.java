9744b61990497ffa7b7c4dc6de5226cb238ef74d
Bing Li <bing.li@snowflake.com>
2020-05-01 13:20:42 -0700

    SNOW-155649 change version to 1.2.4

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -38,7 +38,7 @@ public class Utils
 {
 
   //Connector version, change every release
-  public static final String VERSION = "1.2.3";
+  public static final String VERSION = "1.2.4";
 
   //connector parameter list
   public static final String NAME = "name";
