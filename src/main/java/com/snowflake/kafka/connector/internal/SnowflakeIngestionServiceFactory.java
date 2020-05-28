f77f2428d88d6f8a7abcb90e97762044950b6b0e
sfc-gh-zli <63482288+sfc-gh-zli@users.noreply.github.com>
2020-05-28 12:22:45 -0700

    Minor change to remove hard coded ingestion service port (#151)

--- src/main/java/com/snowflake/kafka/connector/internal/SnowflakeIngestionServiceFactory.java
+++ src/main/java/com/snowflake/kafka/connector/internal/SnowflakeIngestionServiceFactory.java
@@ -11,11 +11,12 @@ public class SnowflakeIngestionServiceFactory
   public static SnowflakeIngestionServiceBuilder builder(String accountName,
                                                          String userName,
                                                          String host,
+                                                         int port,
                                                          String stageName,
                                                          String pipeName,
                                                          PrivateKey privateKey)
   {
-    return new SnowflakeIngestionServiceBuilder(accountName, userName, host,
+    return new SnowflakeIngestionServiceBuilder(accountName, userName, host, port,
       stageName, pipeName, privateKey);
   }
 
@@ -28,12 +29,13 @@ public class SnowflakeIngestionServiceFactory
 
     private SnowflakeIngestionServiceBuilder(String accountName,
                                              String userName, String host,
+                                             int port,
                                              String stageName,
                                              String pipeName,
                                              PrivateKey privateKey)
     {
       this.service = new SnowflakeIngestionServiceV1(accountName, userName,
-        host, stageName, pipeName, privateKey);
+        host, port, stageName, pipeName, privateKey);
     }
 
     SnowflakeIngestionServiceBuilder setTelemetry(SnowflakeTelemetryService telemetry)
