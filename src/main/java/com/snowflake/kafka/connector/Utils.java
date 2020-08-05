00c4132623d1a3264361f425acf6bdf75bfdbaee
Zihan Li <zihan.li@snowflake.com>
2020-08-04 17:36:09 -0700

    SNOW-181035 version change to v1.4.3

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -42,7 +42,7 @@ public class Utils
 {
 
   //Connector version, change every release
-  public static final String VERSION = "1.4.2";
+  public static final String VERSION = "1.4.3";
 
   //connector parameter list
   public static final String NAME = "name";
