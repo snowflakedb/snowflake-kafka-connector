820ac09b38dd8dbb437cf8b929ba7efab4ce1bc2
Jay Patel <jay.patel@snowflake.com>
2021-04-27 09:54:36 -0700

    Example connect-log4j.properties for testing (#297)
    * Example connect-log4j.properties for testing
    
    * Added the same format in test/resources so that our integration tests prints out MDC
    
    * Added example format for IT tests
    
    * Improve logging in retry logic path
    
    - Added enum
    - More logging and details added

---
+++ src/main/java/com/snowflake/kafka/connector/internal/SnowflakeInternalOperations.java
@@ -0,0 +1,26 @@
+package com.snowflake.kafka.connector.internal;
+
+/**
+ * Enum placeholders for internal operations.
+ * Useful mostly in logging and helps figure out where retries are happening.
+ */
+public enum SnowflakeInternalOperations {
+    /* Put api (uploadStream) for GCS */
+    UPLOAD_FILE_TO_INTERNAL_STAGE,
+
+    /* Put API for AWS and Azure which caches the crdentials. */
+    UPLOAD_FILE_TO_INTERNAL_STAGE_NO_CONNECTION,
+
+    /* Broken records or (Failed ingestion files) or (files in internal stage for > 1 hour ) */
+    UPLOAD_FILE_TO_TABLE_STAGE,
+
+    /* Either because of a purge file since they were ingested in SF table or because they were moved to table stage */
+    REMOVE_FILE_FROM_INTERNAL_STAGE,
+
+    /* Snowpipe REST API Usage */
+    INSERT_FILES_SNOWPIPE_API,
+
+    INSERT_REPORT_SNOWPIPE_API,
+
+    LOAD_HISTORY_SCAN_SNOWPIPE_API,
+}
