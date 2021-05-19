db9c07c4315c1c28d1660d161dd78257df16702d
Jay Patel <jay.patel@snowflake.com>
2021-05-18 18:03:54 -0700

    Release SF KC version 1.5.4 (#310)
    * Release SF KC version 1.5.4
    
    - Bumped up dependency (SF JDBC to latest version 3.13.3) Mainly solves
      the problem around failures in putWithconnection API (Returns a
    failure if internal implementation around rest API throws a non 200
    response)
    
    * Remove local changes

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -41,7 +41,7 @@ import org.slf4j.LoggerFactory;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "1.5.3";
+  public static final String VERSION = "1.5.4";
 
   // connector parameter list
   public static final String NAME = "name";
