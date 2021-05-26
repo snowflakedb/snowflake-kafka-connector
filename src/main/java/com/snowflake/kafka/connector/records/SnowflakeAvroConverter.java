0bfe003db67800eaff80da63b59fa5caa8ebbad9
l.heimberg <l.heimberg@cid.com>
2021-05-26 10:23:52 +0200

    Added documentation and logging, refactored parseWithAvroSchema

--- src/main/java/com/snowflake/kafka/connector/records/SnowflakeAvroConverter.java
+++ src/main/java/com/snowflake/kafka/connector/records/SnowflakeAvroConverter.java
@@ -41,11 +41,12 @@ public class SnowflakeAvroConverter extends SnowflakeConverter {
   private SchemaRegistryClient schemaRegistry = null;
 
   public static final String BREAK_ON_SCHEMA_REGISTRY_ERROR = "break.on.schema.registry.error";
-  // By default, we don't break when schema registry is not found
-  private boolean breakOnSchemaRegistryError = false;
-
   public static final String READER_SCHEMA = "reader.schema";
 
+  // By default, we don't break when schema registry is not found
+  private boolean breakOnSchemaRegistryError = false;
+  /* By default, no reader schema is set. In this case, the writer schema of each item is also used
+  as the reader schema. See https://avro.apache.org/docs/1.9.2/spec.html#Schema+Resolution */
   private Schema readerSchema = null;
 
   @Override
@@ -75,6 +76,11 @@ public class SnowflakeAvroConverter extends SnowflakeConverter {
     }
   }
 
+  /**
+   * get reader schema from config if provided
+   *
+   * @param configs configuration for converter
+   */
   void readReaderSchema(final Map<String, ?> configs)
   {
     Object readerSchemaFromConfig = configs.get(READER_SCHEMA);
@@ -92,11 +98,13 @@ public class SnowflakeAvroConverter extends SnowflakeConverter {
       }
       catch (SchemaParseException e)
       {
+        LOGGER.error(Logging.logMessage("the string provided for reader.schema is no valid Avro schema: " + e.getMessage()));
         throw SnowflakeErrors.ERROR_0024.getException(e);
       }
     }
     else
     {
+      LOGGER.error(Logging.logMessage("reader.schema has to be a string"));
       throw SnowflakeErrors.ERROR_0024.getException();
     }
   }
@@ -139,11 +147,11 @@ public class SnowflakeAvroConverter extends SnowflakeConverter {
       return logErrorAndReturnBrokenRecord(e, bytes);
     }
 
-    // If there is any error while getting schema from schema registry,
+    // If there is any error while getting writer schema from schema registry,
     // throw error and break the connector
-    Schema schema;
+    Schema writerSchema;
     try {
-      schema = schemaRegistry.getById(id);
+      writerSchema = schemaRegistry.getById(id);
     } catch (Exception e) {
       if (breakOnSchemaRegistryError) {
         throw SnowflakeErrors.ERROR_0011.getException(e);
@@ -159,7 +167,12 @@ public class SnowflakeAvroConverter extends SnowflakeConverter {
 
       return new SchemaAndValue(
           new SnowflakeJsonSchema(),
-          new SnowflakeRecordContent(parseAvroWithSchema(data, schema), id));
+          new SnowflakeRecordContent(
+                  parseAvroWithSchema(
+                    data,
+                    writerSchema,
+                    readerSchema == null ? writerSchema : readerSchema),
+                  id));
     } catch (Exception e) {
       return logErrorAndReturnBrokenRecord(e, bytes);
     }
@@ -172,23 +185,22 @@ public class SnowflakeAvroConverter extends SnowflakeConverter {
   }
 
   /**
-   * Parse Avro record with schema
+   * Parse Avro record with a writer schema and a reader schema. The writer and the reader schema
+   * have to be compatible as described in https://avro.apache.org/docs/1.9.2/spec.html#Schema+Resolution
    *
    * @param data avro data
-   * @param schema avro schema
+   * @param writerSchema avro schema with which data got serialized
+   * @param readerSchema avro schema that describes the shape of the returned JsonNode
    * @return JsonNode array
    */
-  private JsonNode parseAvroWithSchema(final byte[] data, Schema schema) {
+  private JsonNode parseAvroWithSchema(final byte[] data, Schema writerSchema, Schema readerSchema) {
     final GenericData genericData = new GenericData();
     // Conversion for logical type Decimal. There are conversions for other logical types as well.
     genericData.addLogicalTypeConversion(new Conversions.DecimalConversion());
 
     InputStream is = new ByteArrayInputStream(data);
     Decoder decoder = DecoderFactory.get().binaryDecoder(is, null);
-    DatumReader<GenericRecord> reader = new GenericDatumReader<>(
-            schema,
-            readerSchema == null ? schema : readerSchema,
-            genericData);
+    DatumReader<GenericRecord> reader = new GenericDatumReader<>(writerSchema, readerSchema, genericData);
     try {
       GenericRecord datum = reader.read(null, decoder);
       // For byte data without logical type, this toString method handles it this way:
