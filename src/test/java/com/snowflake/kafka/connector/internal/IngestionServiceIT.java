7aa78f9560749be41e8884f223eb16ae2d79381e
Andy Wang <andy.wang@snowflake.com>
2021-11-29 17:35:18 -0500

    Added tests

--- src/test/java/com/snowflake/kafka/connector/internal/IngestionServiceIT.java
+++ src/test/java/com/snowflake/kafka/connector/internal/IngestionServiceIT.java
@@ -1,6 +1,7 @@
 package com.snowflake.kafka.connector.internal;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import org.junit.After;
@@ -61,5 +62,60 @@ public class IngestionServiceIT {
         4);
 
     assert ingestService.getStageName().equals(stage);
+  }
+
+  @Test
+  public void ingestFileWithClientInfoTestSuccessful() throws Exception {
+    String file = "{\"aa\":123}";
+    String fileName = FileNameUtils.fileName(TestUtils.TEST_CONNECTOR_NAME, table, 0, 0, 1);
+    // Upload a file on internal stage
+    conn.put(stage, fileName, file);
+
+    // Register/Configure a snowpipe client
+    Long clientSequencer = ingestService.configureClient();
+    assert clientSequencer.equals(0l);
+    // Reconfigure the snowpipe client, the clientSequencer should increase
+    clientSequencer = ingestService.configureClient();
+    assert clientSequencer.equals(1l);
+    // Get the client offset token
+    String offsetToken = ingestService.getClientStatus();
+    assert offsetToken == null;
+    // Ingest the file with client info
+    ingestService.ingestFilesWithClientInfo(
+        new ArrayList<>(Arrays.asList(fileName)), clientSequencer);
+    // recreate the file list for ingest report
+    List fileNameList = new ArrayList<>(Arrays.asList(fileName));
+    // make sure ingest is successful
+    TestUtils.assertWithRetry(
+        () -> {
+          Map<String, InternalUtils.IngestedFileStatus> result =
+              ingestService.readIngestReport(fileNameList);
+          return result.get(fileName).equals(InternalUtils.IngestedFileStatus.LOADED);
+        },
+        60,
+        9);
+    // after ingestion, check if the offset token is updated
+    offsetToken = ingestService.getClientStatus();
+    assert offsetToken.equals("1");
+  }
+
+  @Test(expected = SnowflakeKafkaConnectorException.class)
+  public void ingestFileWithClientInfoTestFailed() throws Exception {
+    String file = "{\"aa\":123}";
+    String fileName = FileNameUtils.fileName(TestUtils.TEST_CONNECTOR_NAME, table, 0, 0, 1);
+    // Upload a file on internal stage
+    conn.put(stage, fileName, file);
+
+    // Register/Configure a snowpipe client
+    Long clientSequencer = ingestService.configureClient();
+    assert clientSequencer.equals(0l);
+    // Reconfigure the snowpipe client, the clientSequencer should increase
+    clientSequencer = ingestService.configureClient();
+    assert clientSequencer.equals(1l);
+    // Get the client offset token
+    String offsetToken = ingestService.getClientStatus();
+    assert offsetToken == null;
+    // Ingest the file with outdated client info should throw error
+    ingestService.ingestFilesWithClientInfo(new ArrayList<>(Arrays.asList(fileName)), 0l);
   }
 }
