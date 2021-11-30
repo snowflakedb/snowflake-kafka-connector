5c31bceedfa87da7b4cc4fbed1a3f37951d55c31
Jay Patel <jay.patel@snowflake.com>
2021-11-30 00:49:59 +0000

    Match pom for confluent with pom for apache maven repo (#347)
    - Upgrade version to 1.6.1

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -46,7 +46,7 @@ import org.slf4j.LoggerFactory;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "1.6.0";
+  public static final String VERSION = "1.6.1";
 
   // connector parameter list
   public static final String NAME = "name";
