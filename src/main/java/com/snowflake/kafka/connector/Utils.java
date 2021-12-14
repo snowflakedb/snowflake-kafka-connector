43b1f0a730b03c600959546d522f55bfacd5db6a
Jay Patel <jay.patel@snowflake.com>
2021-12-14 23:48:45 +0530

    release KC 1.6.3 - log4j vulnerability + JDBC upgrade

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -46,7 +46,7 @@ import org.slf4j.LoggerFactory;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "1.6.2";
+  public static final String VERSION = "1.6.3";
 
   // connector parameter list
   public static final String NAME = "name";
