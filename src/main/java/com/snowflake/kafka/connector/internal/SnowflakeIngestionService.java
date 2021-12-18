026a504a578bf4cf1421502c52762fd4094cca43
Andy Wang <andy.wang@snowflake.com>
2021-12-18 12:31:57 -0500

    SNOW-508731 update snowflake ingestion service (#350)
    * Add new Ingestion Service api
    
    * Added tests
    
    * update comments
    
    * add ignore the new unit tests
    
    * fix type
    
    * resolve comments
    
    * Update javadoc
    
    * Update test
    
    * Update ingest sdk to 0.10.4-beta for new APIs
    
    * Enable IngestServiceIT test
    
    Co-authored-by: Jay Patel <jay.patel@snowflake.com>

--- src/main/java/com/snowflake/kafka/connector/internal/SnowflakeIngestionService.java
+++ src/main/java/com/snowflake/kafka/connector/internal/SnowflakeIngestionService.java
@@ -3,6 +3,8 @@ package com.snowflake.kafka.connector.internal;
 import com.snowflake.kafka.connector.internal.InternalUtils.IngestedFileStatus;
 import java.util.List;
 import java.util.Map;
+import net.snowflake.ingest.connection.ClientStatusResponse;
+import net.snowflake.ingest.connection.ConfigureClientResponse;
 
 /** Ingestion service manages snow pipe */
 public interface SnowflakeIngestionService {
@@ -45,6 +47,28 @@ public interface SnowflakeIngestionService {
    * @return a map contains all file status
    */
   Map<String, IngestedFileStatus> readOneHourHistory(List<String> files, long startTime);
+
+  /**
+   * configure the Snowpipe client and return the client sequencer
+   *
+   * @return ConfigureClientResponse contains the client sequencer
+   */
+  ConfigureClientResponse configureClient();
+
+  /**
+   * get the Snowpipe client and return the ConfigureClientResponse
+   *
+   * @return ConfigureClientResponse contains the offset token (nullable) and client sequencer
+   */
+  ClientStatusResponse getClientStatus();
+
+  /**
+   * Ingest a list of files with the clientInfo (clientSequencer and offsetToken)
+   *
+   * @param fileNames file name List
+   * @param clientSequencer unique identification of the Snowpipe client
+   */
+  void ingestFilesWithClientInfo(List<String> fileNames, long clientSequencer);
 
   /** close ingest service */
   void close();
