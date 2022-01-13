440ef215cf280b7fc20e438a15cd5eab4e6c5248
Jay Patel <jay.patel@snowflake.com>
2022-01-13 13:08:43 +0530

    Release version 1.6.4
    - JDBC 3.13.13
    - log4j-core 2.17.1

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -46,7 +46,7 @@ import org.slf4j.LoggerFactory;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "1.6.3";
+  public static final String VERSION = "1.6.4";
 
   // connector parameter list
   public static final String NAME = "name";
