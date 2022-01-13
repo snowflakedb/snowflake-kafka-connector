647a8ac0419d2b3e89556a9f2a02e6e53894f0cb
Jay Patel <jay.patel@snowflake.com>
2022-01-14 00:32:13 +0530

    Release version 1.7.0 (#376)
    Notes:
    - New config for delivery guarantee
      - delivery.guarantee = AT_LEAST_ONCE/EXACTLY_ONCE

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -47,7 +47,7 @@ import org.slf4j.LoggerFactory;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "1.6.1";
+  public static final String VERSION = "1.7.0";
 
   // connector parameter list
   public static final String NAME = "name";
