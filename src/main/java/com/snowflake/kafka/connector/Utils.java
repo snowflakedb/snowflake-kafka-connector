c04c0abb990d6ce5062d435d96dd1c57b26a81ca
Jay Patel <jay.patel@snowflake.com>
2022-01-22 14:10:08 +0530

    Release branch 1.7.1 (#386)
    * Update jackson core and databind to 2.12.6
    
    * Release version 1.7.1
    
    - JDBC version 3.13.14
    - jackson-core and jackson-databind 2.12.6 for resolving https://github.com/FasterXML/jackson-databind/issues/3328

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -47,7 +47,7 @@ import org.slf4j.LoggerFactory;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "1.7.0";
+  public static final String VERSION = "1.7.1";
 
   // connector parameter list
   public static final String NAME = "name";
