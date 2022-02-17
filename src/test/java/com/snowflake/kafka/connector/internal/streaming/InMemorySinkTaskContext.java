0df1c71c24b511657ba9d78f23fc364affcb93f1
Jay Patel <jay.patel@snowflake.com>
2022-02-18 01:31:44 +0530

    SNOW-451197 replay offsets when offset token fails even after retries (#400)
    * Pass in sinktaskcontext for replaying offsets in case of failures
    
    * Pass TopicPartition to TopicPartitionChannel for resetting the offset
    
    - Refactoring startTask method in SnowflakeSinkService.
    - Instead of passing topic and partition separately, pass TopicPartition
      and let the implementation fetch topic and partition.
    - No behavior change in SnowflakeSinkServiceV1.
    - Called sinkTaskContext's offset API to reset the committed offset in
      case of failures in getOffsetToken API
    - Removed onSuccess listener of fallback since that is called even if
      Fallback is not executed. (This is how it is implemented and can
      create confusions while reading logs.)
    
    * Add Javadocs
    
    * Reset offset for topicPartition after recovery
    
    - After exhausting retries for offsetToken, reopen the channel and
      refetch the offsetToken.
    - Use this (offset+1) to reset this offset.
    
    * Test changes only
    
    * Replace the channel object after reopening to not use the stale instance for refetching the offsetToken
    
    * Add IT test for reopening channel
    
    - Move Mocked implementations into its own class.
    
    * Log debugger changes
    
    - Minor revert of unwanted changes.
    
    * Fix test, add one more test in TopicPartitionChannelIT for closed channel
    
    * Addressed review comments
    
    * Addressed review comments- minor javadocs

---
+++ src/test/java/com/snowflake/kafka/connector/internal/streaming/InMemorySinkTaskContext.java
@@ -0,0 +1,61 @@
+package com.snowflake.kafka.connector.internal.streaming;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.connect.sink.ErrantRecordReporter;
+import org.apache.kafka.connect.sink.SinkTaskContext;
+
+/* In memory implementation of SinkTaskContext used for testing */
+public class InMemorySinkTaskContext implements SinkTaskContext {
+  private final Map<TopicPartition, Long> offsets = new HashMap();
+  private long timeoutMs = -1L;
+  private Set<TopicPartition> assignment;
+
+  public InMemorySinkTaskContext(Set<TopicPartition> assignment) {
+    this.assignment = assignment;
+  }
+
+  public Map<String, String> configs() {
+    throw new UnsupportedOperationException();
+  }
+
+  public void offset(Map<TopicPartition, Long> offsets) {
+    this.offsets.putAll(offsets);
+  }
+
+  public void offset(TopicPartition tp, long offset) {
+    this.offsets.put(tp, offset);
+  }
+
+  public Map<TopicPartition, Long> offsets() {
+    return this.offsets;
+  }
+
+  public void timeout(long timeoutMs) {
+    this.timeoutMs = timeoutMs;
+  }
+
+  public long timeout() {
+    return this.timeoutMs;
+  }
+
+  public Set<TopicPartition> assignment() {
+    return this.assignment;
+  }
+
+  public void setAssignment(Set<TopicPartition> nextAssignment) {
+    this.assignment = nextAssignment;
+  }
+
+  public void pause(TopicPartition... partitions) {}
+
+  public void resume(TopicPartition... partitions) {}
+
+  public void requestCommit() {}
+
+  public ErrantRecordReporter errantRecordReporter() {
+    throw new UnsupportedOperationException("ErrantRecordReporter is undefined for this class");
+  }
+}
