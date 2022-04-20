9e8c8aad79eea2e0025a57a6021a0dbff9f0a23c
Jay Patel <jay.patel@snowflake.com>
2022-04-20 14:15:01 -0700

    Update ingest sdk and release 1.5.6 KC

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -45,7 +45,7 @@ import org.slf4j.LoggerFactory;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "1.5.5";
+  public static final String VERSION = "1.5.6";
 
   // connector parameter list
   public static final String NAME = "name";
