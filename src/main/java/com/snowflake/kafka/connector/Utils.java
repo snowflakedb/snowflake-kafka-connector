eb05384db11b5ad38059da0622e0fadee2417b83
Jay Patel <jay.patel@snowflake.com>
2022-04-28 11:39:16 -0700

    SNOW-279791 Release version 1.8.0 (#425)
    Private Preview for Streaming Snowpipe + KC 😉 🤩

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -51,7 +51,7 @@ import org.slf4j.LoggerFactory;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "1.7.1";
+  public static final String VERSION = "1.8.0";
 
   // connector parameter list
   public static final String NAME = "name";
