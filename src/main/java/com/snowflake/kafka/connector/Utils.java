71d29a29d144bad419f8c12961d1f388c259fe26
Jay Patel <jay.patel@snowflake.com>
2022-06-09 11:20:23 -0700

    Release 1.7.3 with 0.10.4-beta.3 - More connections in connections pool

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -47,7 +47,7 @@ import org.slf4j.LoggerFactory;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "1.7.2";
+  public static final String VERSION = "1.7.3";
 
   // connector parameter list
   public static final String NAME = "name";
