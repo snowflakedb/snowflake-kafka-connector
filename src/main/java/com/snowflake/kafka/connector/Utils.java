ef44b9a51fa147e61e94f41fc34a2255837bbbd7
Zekai Fan <zekai.fan@snowflake.com>
2022-07-27 17:26:18 -0700

    random change

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -364,13 +364,14 @@ public class Utils {
       if (!BufferThreshold.validateBufferThreshold(config, IngestionMethodConfig.SNOWPIPE)) {
         configIsValid = false;
       }
-      if (config.containsKey(SnowflakeSinkConnectorConfig.SCHEMATIZATION_ENABLE_CONFIG) && Boolean.parseBoolean(
-                config.get(SnowflakeSinkConnectorConfig.SCHEMATIZATION_ENABLE_CONFIG))) {
-          configIsValid = false;
-          LOGGER.error(
-              Logging.logMessage(
-                  "Schematization is only available with {}.",
-                  IngestionMethodConfig.SNOWPIPE_STREAMING.toString()));
+      if (config.containsKey(SnowflakeSinkConnectorConfig.SCHEMATIZATION_ENABLE_CONFIG)
+          && Boolean.parseBoolean(
+              config.get(SnowflakeSinkConnectorConfig.SCHEMATIZATION_ENABLE_CONFIG))) {
+        configIsValid = false;
+        LOGGER.error(
+            Logging.logMessage(
+                "Schematization is only available with {}.",
+                IngestionMethodConfig.SNOWPIPE_STREAMING.toString()));
       }
     }
 
