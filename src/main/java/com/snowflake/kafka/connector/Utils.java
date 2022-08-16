58fa3ca5136ae96edd7d69be0827dbe79cfe0aed
Jay Patel <jay.patel@snowflake.com>
2022-08-16 12:57:55 -0700

    Release 1.6.8

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -46,7 +46,7 @@ import org.slf4j.LoggerFactory;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "1.6.7";
+  public static final String VERSION = "1.6.8";
 
   // connector parameter list
   public static final String NAME = "name";
