ee7d2a05f042f75567c4b5f828056d6c6d6ff0ee
revi cheng <revi.cheng@snowflake.com>
2023-01-13 13:28:03 -0800

    adding start start log lol

--- src/main/java/com/snowflake/kafka/connector/SnowflakeSinkTask.java
+++ src/main/java/com/snowflake/kafka/connector/SnowflakeSinkTask.java
@@ -161,6 +161,8 @@ public class SnowflakeSinkTask extends SinkTask {
 
     this.DYNAMIC_LOGGER.setLoggerInstanceTag(this.getTaskLoggingTag());
 
+    this.DYNAMIC_LOGGER.info("starting task...");
+    
     // generate topic to table map
     this.topic2table = getTopicToTableMap(parsedConfig);
 
