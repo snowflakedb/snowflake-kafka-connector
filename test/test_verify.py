f98d2077cbad1a1e5641b6a9c5da2afe4dd520b7
Jay Patel <jay.patel@snowflake.com>
2023-03-16 09:54:11 -0700

    Disable delivery guarantee tests

--- test/test_verify.py
+++ test/test_verify.py
@@ -703,6 +703,6 @@ if __name__ == "__main__":
                           testVersion,
                           enableSSL,
                           snowflakeCloudPlatform,
-                          enableDeliveryGuaranteeTests)
+                          False)
 
     runTestSet(kafkaTest, testSet, nameSalt, pressure)
