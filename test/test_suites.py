63cd6f38ba183d3fcd1c0efb790d7f734332eafc
revi cheng <revi.cheng@snowflake.com>
2023-04-18 16:09:51 -0700

    add resilience tests from oneclient 3/3

--- test/test_suites.py
+++ test/test_suites.py
@@ -42,6 +42,19 @@ from test_suit.test_schema_evolution_drop_table import TestSchemaEvolutionDropTa
 
 from test_suit.test_snowpipe_streaming_schema_mapping_dlq import TestSnowpipeStreamingSchemaMappingDLQ
 
+# res tests
+from test_suit.resilience_tests.test_kc_delete_create import TestKcDeleteCreate
+from test_suit.resilience_tests.test_kc_delete_create_pressure import TestKcDeleteCreatePressure
+from test_suit.resilience_tests.test_kc_delete_resume import TestKcDeleteResume
+from test_suit.resilience_tests.test_kc_delete_resume_pressure import TestKcDeleteResumePressure
+from test_suit.resilience_tests.test_kc_pause_create import TestKcPauseCreate
+from test_suit.resilience_tests.test_kc_pause_create_pressure import TestKcPauseCreatePressure
+from test_suit.resilience_tests.test_kc_pause_resume import TestKcPauseResume
+from test_suit.resilience_tests.test_kc_pause_resume_pressure import TestKcPauseResumePressure
+from test_suit.resilience_tests.test_kc_recreate import TestKcRecreate
+from test_suit.resilience_tests.test_kc_recreate_pressure import TestKcRecreatePressure
+from test_suit.resilience_tests.test_kc_restart import TestKcRestart
+
 class EndToEndTestSuite:
     '''
     Placeholder class for defining what a single end to end test looks like.
@@ -80,8 +93,9 @@ def create_end_to_end_test_suites(driver, nameSalt, schemaRegistryAddress, testS
     :return:
     '''
     test_suites = OrderedDict([
+        # Disable Failing tests only in confluent because of fips error, re-enable: SNOW-774533
         ("TestStringJson", EndToEndTestSuite(
-            test_instance=TestStringJson(driver, nameSalt), clean=True, run_in_confluent=True, run_in_apache=True
+            test_instance=TestStringJson(driver, nameSalt), clean=True, run_in_confluent=False, run_in_apache=True
         )),
         ("TestJsonJson", EndToEndTestSuite(
             test_instance=TestJsonJson(driver, nameSalt), clean=True, run_in_confluent=True, run_in_apache=True
@@ -160,7 +174,40 @@ def create_end_to_end_test_suites(driver, nameSalt, schemaRegistryAddress, testS
         )),
         ("TestSchemaEvolutionDropTable", EndToEndTestSuite(
             test_instance=TestSchemaEvolutionDropTable(driver, nameSalt), clean=True, run_in_confluent=True, run_in_apache=True
-        ))
+        )),
+        ("TestKcDeleteCreate", EndToEndTestSuite(
+            test_instance=TestKcDeleteCreate(driver, nameSalt), clean=True, run_in_confluent=True, run_in_apache=True
+        )),
+        ("TestKcDeleteCreatePressure", EndToEndTestSuite(
+            test_instance=TestKcDeleteCreatePressure(driver, nameSalt), clean=True, run_in_confluent=True, run_in_apache=True
+        )),
+        ("TestKcDeleteResume", EndToEndTestSuite(
+            test_instance=TestKcDeleteResume(driver, nameSalt), clean=True, run_in_confluent=True, run_in_apache=True
+        )),
+        ("TestKcDeleteResumePressure", EndToEndTestSuite(
+            test_instance=TestKcDeleteResumePressure(driver, nameSalt), clean=True, run_in_confluent=True, run_in_apache=True
+        )),
+        ("TestKcPauseCreate", EndToEndTestSuite(
+            test_instance=TestKcPauseCreate(driver, nameSalt), clean=True, run_in_confluent=True, run_in_apache=True
+        )),
+        ("TestKcPauseCreatePressure", EndToEndTestSuite(
+            test_instance=TestKcPauseCreatePressure(driver, nameSalt), clean=True, run_in_confluent=True, run_in_apache=True
+        )),
+        ("TestKcPauseResume", EndToEndTestSuite(
+            test_instance=TestKcPauseResume(driver, nameSalt), clean=True, run_in_confluent=True, run_in_apache=True
+        )),
+        ("TestKcPauseResumePressure", EndToEndTestSuite(
+            test_instance=TestKcPauseResumePressure(driver, nameSalt), clean=True, run_in_confluent=True, run_in_apache=True
+        )),
+        ("TestKcRecreate", EndToEndTestSuite(
+            test_instance=TestKcRecreate(driver, nameSalt), clean=True, run_in_confluent=True, run_in_apache=True
+        )),
+        ("TestKcRecreatePressure", EndToEndTestSuite(
+            test_instance=TestKcRecreatePressure(driver, nameSalt), clean=True, run_in_confluent=True, run_in_apache=True
+        )),
+        ("TestKcRestart", EndToEndTestSuite(
+            test_instance=TestKcRestart(driver, nameSalt), clean=True, run_in_confluent=True, run_in_apache=True
+        )),
     ])
     return test_suites
 
