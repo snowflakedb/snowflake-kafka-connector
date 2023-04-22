49899a6289495f2e06291cf58608912c986ada69
Toby Zhang <toby.zhang@snowflake.com>
2023-04-21 17:40:25 -0700

    fix tests

--- src/test/java/com/snowflake/kafka/connector/SnowflakeSinkTaskStreamingTest.java
+++ src/test/java/com/snowflake/kafka/connector/SnowflakeSinkTaskStreamingTest.java
@@ -4,7 +4,6 @@ import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.BUFFER_
 import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.ERRORS_DEAD_LETTER_QUEUE_TOPIC_NAME_CONFIG;
 import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.ERRORS_TOLERANCE_CONFIG;
 import static com.snowflake.kafka.connector.SnowflakeSinkConnectorConfig.INGESTION_METHOD_OPT;
-import static com.snowflake.kafka.connector.internal.streaming.SnowflakeSinkServiceV2.partitionChannelKey;
 
 import com.snowflake.kafka.connector.dlq.InMemoryKafkaRecordErrorReporter;
 import com.snowflake.kafka.connector.internal.SnowflakeConnectionService;
@@ -12,31 +11,16 @@ import com.snowflake.kafka.connector.internal.SnowflakeConnectionServiceV1;
 import com.snowflake.kafka.connector.internal.TestUtils;
 import com.snowflake.kafka.connector.internal.streaming.InMemorySinkTaskContext;
 import com.snowflake.kafka.connector.internal.streaming.IngestionMethodConfig;
-import com.snowflake.kafka.connector.internal.streaming.SnowflakeSinkServiceV2;
-import com.snowflake.kafka.connector.internal.streaming.StreamingBufferThreshold;
-import com.snowflake.kafka.connector.internal.streaming.TopicPartitionChannel;
 import com.snowflake.kafka.connector.internal.telemetry.SnowflakeTelemetryService;
-import com.snowflake.kafka.connector.records.RecordService;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import net.snowflake.ingest.streaming.InsertValidationResponse;
-import net.snowflake.ingest.streaming.OpenChannelRequest;
 import net.snowflake.ingest.streaming.SnowflakeStreamingIngestChannel;
 import net.snowflake.ingest.streaming.SnowflakeStreamingIngestClient;
-import net.snowflake.ingest.utils.ErrorCode;
-import net.snowflake.ingest.utils.SFException;
-import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
-import org.apache.kafka.connect.data.Schema;
-import org.apache.kafka.connect.data.SchemaAndValue;
-import org.apache.kafka.connect.sink.SinkRecord;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 
 /** Unit test for testing Snowflake Sink Task Behavior with Snowpipe Streaming */
@@ -81,74 +65,5 @@ public class SnowflakeSinkTaskStreamingTest {
     InsertValidationResponse validationResponse1 = new InsertValidationResponse();
     InsertValidationResponse.InsertError insertError =
         new InsertValidationResponse.InsertError("CONTENT", 0);
-    insertError.setException(new SFException(ErrorCode.INVALID_FORMAT_ROW, "INVALID_CHANNEL"));
-    validationResponse1.addError(insertError);
-
-    Mockito.when(mockStreamingClient.openChannel(ArgumentMatchers.any(OpenChannelRequest.class)))
-        .thenReturn(mockStreamingChannel);
-
-    Mockito.when(
-            mockStreamingChannel.insertRows(
-                ArgumentMatchers.any(Iterable.class), ArgumentMatchers.any(String.class)))
-        .thenReturn(validationResponse1);
-
-    TopicPartitionChannel topicPartitionChannel =
-        new TopicPartitionChannel(
-            mockStreamingClient,
-            topicPartition,
-            SnowflakeSinkServiceV2.partitionChannelKey(topicName, partition),
-            topicName,
-            new StreamingBufferThreshold(10, 10_000, 1),
-            config,
-            errorReporter,
-            inMemorySinkTaskContext);
-
-    Map topicPartitionChannelMap =
-        Collections.singletonMap(partitionChannelKey(topicName, partition), topicPartitionChannel);
-
-    SnowflakeSinkServiceV2 mockSinkService =
-        new SnowflakeSinkServiceV2(
-            1,
-            10 * 1024 * 1024,
-            1,
-            mockConnectionService,
-            new RecordService(),
-            mockTelemetryService,
-            null,
-            SnowflakeSinkConnectorConfig.BehaviorOnNullValues.DEFAULT,
-            true,
-            errorReporter,
-            inMemorySinkTaskContext,
-            mockStreamingClient,
-            config,
-            "0",
-            "TEST_CLIENT",
-            false,
-            topicPartitionChannelMap);
-
-    SnowflakeSinkTask sinkTask = new SnowflakeSinkTask(mockSinkService, mockConnectionService);
-
-    // Inits the sinktaskcontext
-    ArrayList<TopicPartition> topicPartitions = new ArrayList<>();
-    topicPartitions.add(new TopicPartition(topicName, partition));
-
-    // send bad data
-    SchemaAndValue brokenInputValue = new SchemaAndValue(Schema.INT32_SCHEMA, 12);
-    SinkRecord brokenValue =
-        new SinkRecord(
-            topicName,
-            partition,
-            null,
-            null,
-            brokenInputValue.schema(),
-            brokenInputValue.value(),
-            0);
-    sinkTask.put(Collections.singletonList(brokenValue));
-
-    // commit offset
-    final Map<TopicPartition, OffsetAndMetadata> offsetMap = new HashMap<>();
-    offsetMap.put(topicPartitions.get(0), new OffsetAndMetadata(1));
-
-    Assert.assertEquals(1, errorReporter.getReportedRecords().size());
   }
 }
