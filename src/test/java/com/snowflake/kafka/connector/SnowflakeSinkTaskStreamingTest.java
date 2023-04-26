d87fb28d37badb3d1fa7c6bea38ab9917f8c1e8e
Toby Zhang <toby.zhang@snowflake.com>
2023-04-26 16:48:08 -0700

    NO-SNOW: fix offset token behavior when resetting the offset to data that expired (#619)
    - Remove the change that catch InvalidOffsetException during reseting the offset since the suggestion of setting auto.reset.offset doesn't really work
    - Add back isOffsetResetInKafka with modifications to support exactly once delivery for a special case

--- src/test/java/com/snowflake/kafka/connector/SnowflakeSinkTaskStreamingTest.java
+++ src/test/java/com/snowflake/kafka/connector/SnowflakeSinkTaskStreamingTest.java
@@ -40,7 +40,6 @@ import org.mockito.Mockito;
 
 /** Unit test for testing Snowflake Sink Task Behavior with Snowpipe Streaming */
 public class SnowflakeSinkTaskStreamingTest {
-
   private String topicName;
   private static int partition = 0;
   private TopicPartition topicPartition;
@@ -56,26 +55,19 @@ public class SnowflakeSinkTaskStreamingTest {
     Map<String, String> config = TestUtils.getConfForStreaming();
     SnowflakeSinkConnectorConfig.setDefaultValues(config);
     config.put(BUFFER_COUNT_RECORDS, "1"); // override
-
     config.put(INGESTION_METHOD_OPT, IngestionMethodConfig.SNOWPIPE_STREAMING.toString());
-
     config.put(ERRORS_TOLERANCE_CONFIG, SnowflakeSinkConnectorConfig.ErrorTolerance.ALL.toString());
     config.put(ERRORS_DEAD_LETTER_QUEUE_TOPIC_NAME_CONFIG, "test_DLQ");
-
     InMemoryKafkaRecordErrorReporter errorReporter = new InMemoryKafkaRecordErrorReporter();
-
     SnowflakeConnectionService mockConnectionService =
         Mockito.mock(SnowflakeConnectionServiceV1.class);
-
     SnowflakeStreamingIngestClient mockStreamingClient =
         Mockito.mock(SnowflakeStreamingIngestClient.class);
     SnowflakeTelemetryService mockTelemetryService = Mockito.mock(SnowflakeTelemetryService.class);
     InMemorySinkTaskContext inMemorySinkTaskContext =
         new InMemorySinkTaskContext(Collections.singleton(topicPartition));
-
     SnowflakeStreamingIngestChannel mockStreamingChannel =
         Mockito.mock(SnowflakeStreamingIngestChannel.class);
-
     InsertValidationResponse validationResponse1 = new InsertValidationResponse();
     InsertValidationResponse.InsertError insertError =
         new InsertValidationResponse.InsertError("CONTENT", 0);
