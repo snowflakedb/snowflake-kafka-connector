eec93c0c8c2fbf3b9788c42658d0ee45d2da90f7
Jay Patel <jay.patel@snowflake.com>
2023-04-29 00:04:02 +0000

    NO-SNOW Release version 1.9.2 (#624)

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -49,7 +49,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "1.9.1";
+  public static final String VERSION = "1.9.2";
 
   // connector parameter list
   public static final String NAME = "name";
