5ae269cd3c3825f2c314da51cb4f9f87df02c7e6
CharlieFirth <charlie.firth@checkout.com>
2023-05-18 13:45:09 +0100

    pomconfluent

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -50,7 +50,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "1.9.0";
+  public static final String VERSION = "1.8.4-CKO";
 
   // connector parameter list
   public static final String NAME = "name";
