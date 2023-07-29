c7889ea46377de35bd754e8457676a079e1d371b
revi cheng <revi.cheng@snowflake.com>
2023-07-29 00:26:07 -0700

    Create release v2.0.0 for GA (#676)
    Co-authored-by: Jay Patel <jay.patel@snowflake.com>

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -50,7 +50,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "1.9.4";
+  public static final String VERSION = "2.0.0";
 
   // connector parameter list
   public static final String NAME = "name";
