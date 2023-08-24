06c69baa1ea2dc7644d0614509a8a4b4f64b7f02
revi cheng <revi.cheng@snowflake.com>
2023-08-24 16:24:51 -0700

    group it test

--- src/test/java/com/snowflake/kafka/connector/internal/TombstoneRecordIngestionIT.java
+++ src/test/java/com/snowflake/kafka/connector/internal/TombstoneRecordIngestionIT.java
@@ -131,11 +131,6 @@ public class TombstoneRecordIngestionIT {
       connectorConfig = TestUtils.getConfForStreaming();
     }
 
-    // set default behavior
-    connectorConfig.put(
-        SnowflakeSinkConnectorConfig.BEHAVIOR_ON_NULL_VALUES_CONFIG,
-        SnowflakeSinkConnectorConfig.BehaviorOnNullValues.DEFAULT.toString());
-
     TopicPartition topicPartition = new TopicPartition(topic, partition);
     SnowflakeSinkService service =
         SnowflakeSinkServiceFactory.builder(conn, this.ingestionMethod, connectorConfig)
@@ -145,64 +140,32 @@ public class TombstoneRecordIngestionIT {
             .addTask(table, topicPartition)
             .build();
 
-    // make tombstone record
+    // make tombstone records
     SchemaAndValue input = converter.toConnectData(topic, null);
-    long offset = 0;
-    SinkRecord record1 =
+    SinkRecord tombstoneRecord1 =
         new SinkRecord(
-            topic, partition, Schema.STRING_SCHEMA, "test", input.schema(), input.value(), offset);
+            topic, partition, Schema.STRING_SCHEMA, "tombstoneRecord1", input.schema(), input.value(), 0);
+    SinkRecord tombstoneRecord2 =
+        new SinkRecord(
+            topic, partition, Schema.STRING_SCHEMA, "tombstoneRecord2", null, null, 1);
+    SinkRecord allNullRecord1 =
+        new SinkRecord(
+            topic, partition, null, null, null, null, 2);
+    SinkRecord allNullRecord2 =
+        new SinkRecord(
+            topic, partition, null, null, input.schema(), input.value(), 3);
+    SinkRecord allNullRecord3 =
+        new SinkRecord(
+            topic, partition, input.schema(), input.value(), input.schema(), input.value(), 4);
 
     // test insert
-    service.insert(Collections.singletonList(record1));
+    service.insert(Arrays.asList(tombstoneRecord1, tombstoneRecord2, allNullRecord1, allNullRecord2, allNullRecord3));
     service.callAllGetOffset();
 
     // verify inserted
-    TestUtils.assertWithRetry(() -> TestUtils.tableSize(table) == 1, 30, 20);
+    TestUtils.assertWithRetry(() -> TestUtils.tableSize(table) == 5, 30, 20);
     TestUtils.assertWithRetry(
-        () -> service.getOffset(new TopicPartition(topic, partition)) == 1, 20, 5);
-
-    service.closeAll();
-  }
-
-  @Test
-  public void testDefaultAllNullTombstoneRecordBehavior() throws Exception {
-    Map<String, String> connectorConfig = TestUtils.getConfig();
-
-    if (this.ingestionMethod.equals(IngestionMethodConfig.SNOWPIPE)) {
-      conn.createTable(table);
-      conn.createStage(stage);
-    } else {
-      connectorConfig = TestUtils.getConfForStreaming();
-    }
-
-    // set default behavior
-    connectorConfig.put(
-        SnowflakeSinkConnectorConfig.BEHAVIOR_ON_NULL_VALUES_CONFIG,
-        SnowflakeSinkConnectorConfig.BehaviorOnNullValues.DEFAULT.toString());
-
-    TopicPartition topicPartition = new TopicPartition(topic, partition);
-    SnowflakeSinkService service =
-        SnowflakeSinkServiceFactory.builder(conn, this.ingestionMethod, connectorConfig)
-            .setRecordNumber(1)
-            .setErrorReporter(new InMemoryKafkaRecordErrorReporter())
-            .setSinkTaskContext(new InMemorySinkTaskContext(Collections.singleton(topicPartition)))
-            .addTask(table, topicPartition)
-            .build();
-
-    // make tombstone record
-    SchemaAndValue input = converter.toConnectData(topic, null);
-    long offset = 0;
-    SinkRecord record1 =
-        new SinkRecord(topic, partition, null, null, input.schema(), input.value(), offset);
-
-    // test insert
-    service.insert(Collections.singletonList(record1));
-    service.callAllGetOffset();
-
-    // verify inserted
-    TestUtils.assertWithRetry(() -> TestUtils.tableSize(table) == 1, 30, 20);
-    TestUtils.assertWithRetry(
-        () -> service.getOffset(new TopicPartition(topic, partition)) == 1, 20, 5);
+        () -> service.getOffset(new TopicPartition(topic, partition)) == 5, 20, 5);
 
     service.closeAll();
   }
@@ -225,10 +188,8 @@ public class TombstoneRecordIngestionIT {
             .setErrorReporter(new InMemoryKafkaRecordErrorReporter())
             .setSinkTaskContext(new InMemorySinkTaskContext(Collections.singleton(topicPartition)))
             .addTask(table, topicPartition)
+            .setBehaviorOnNullValuesConfig(SnowflakeSinkConnectorConfig.BehaviorOnNullValues.IGNORE)
             .build();
-
-    // set ignore behavior
-    service.setBehaviorOnNullValuesConfig(SnowflakeSinkConnectorConfig.BehaviorOnNullValues.IGNORE);
 
     // make tombstone record
     SchemaAndValue record1Input = converter.toConnectData(topic, null);
