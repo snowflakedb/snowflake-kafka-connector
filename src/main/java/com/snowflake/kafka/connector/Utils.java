33eb93ec63d556e143b489be5414b7d0fb805890
revi cheng <revi.cheng@snowflake.com>
2023-09-25 16:31:21 -0700

    Bump KC version for new 2.1.0 release (#707)

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -71,7 +71,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "2.0.1";
+  public static final String VERSION = "2.1.0";
 
   // connector parameter list
   public static final String NAME = "name";
