64b126814a1561092093900460cb94e368f8949b
Jay Patel <jay.patel@snowflake.com>
2023-11-08 11:25:04 -0800

    2.0.2-rc

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -50,7 +50,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "2.0.1";
+  public static final String VERSION = "2.0.2-rc";
 
   // connector parameter list
   public static final String NAME = "name";
