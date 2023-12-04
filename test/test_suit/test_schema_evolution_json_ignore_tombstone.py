76ecc7cebd2c4be6e74df6b48846c6a40a4cecb7
Khyati Soneji <134907874+khsoneji@users.noreply.github.com>
2023-12-04 12:53:22 +0530

    Revert "Revert commits (#46)" (#48)
    This reverts commit bd64a9235de4cbccd8e59795b148ebd59b9912d9.

--- test/test_suit/test_schema_evolution_json_ignore_tombstone.py
+++ test/test_suit/test_schema_evolution_json_ignore_tombstone.py
@@ -55,15 +55,17 @@ class TestSchemaEvolutionJsonIgnoreTombstone:
             key = []
             value = []
 
-            # send one less record because we are sending a tombstone record. tombstone ingestion is enabled by default
-            for e in range(self.recordNum - 1):
+            # send two less record because we are sending tombstone records. tombstone ingestion is enabled by default
+            for e in range(self.recordNum - 2):
                 key.append(json.dumps({'number': str(e)}).encode('utf-8'))
                 value.append(json.dumps(self.records[i]).encode('utf-8'))
 
             # append tombstone except for 2.5.1 due to this bug: https://issues.apache.org/jira/browse/KAFKA-10477
             if self.driver.testVersion != '2.5.1':
-                value.append('')
-            key.append(json.dumps({'number': str(i)}).encode('utf-8'))
+                key.append(json.dumps({'number': str(self.recordNum - 1)}).encode('utf-8'))
+                value.append(None)
+                key.append(json.dumps({'number': str(self.recordNum)}).encode('utf-8'))
+                value.append("") # community converters treat this as a tombstone
 
             self.driver.sendBytesData(topic, value, key)
 
@@ -87,7 +89,7 @@ class TestSchemaEvolutionJsonIgnoreTombstone:
 
         res = self.driver.snowflake_conn.cursor().execute(
             "SELECT count(*) FROM {}".format(self.table)).fetchone()[0]
-        if res != len(self.topics) * (self.recordNum - 1):
+        if res != len(self.topics) * (self.recordNum - 2):
             print("Number of record expected: {}, got: {}".format(len(self.topics) * (self.recordNum - 1), res))
             raise NonRetryableError("Number of record in table is different from number of record sent")
 
