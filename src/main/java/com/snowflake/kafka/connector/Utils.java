ea87fec7c9e3c011fe59df6923117772c5d3962b
revi cheng <revi.cheng@snowflake.com>
2024-01-30 11:02:42 -0800

    Update version to 2.2.0 for release (#779)

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -71,7 +71,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "2.1.2";
+  public static final String VERSION = "2.2.0";
 
   // connector parameter list
   public static final String NAME = "name";
