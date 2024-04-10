2c5a6aacc8df086f15ad3f025bea1b47232f5924
Adrian Kowalczyk <adrian.kowalczyk@snowflake.com>
2024-04-10 15:12:53 +0200

    Some refactoring of IT

--- src/test/java/com/snowflake/kafka/connector/SmtIT.java
+++ src/test/java/com/snowflake/kafka/connector/SmtIT.java
@@ -11,10 +11,28 @@ import java.util.Map;
 import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 import org.apache.kafka.connect.json.JsonConverter;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class SmtIT extends ConnectClusterBaseIT {
+
+  private String smtTopic;
+  private String smtConnector;
+
+  @BeforeEach
+  void before() {
+    smtTopic = TestUtils.randomTableName();
+    smtConnector = String.format("%s_connector", smtTopic);
+    connectCluster.kafka().createTopic(smtTopic);
+  }
+
+  @AfterEach
+  void after() {
+    connectCluster.kafka().deleteTopic(smtTopic);
+    connectCluster.deleteConnector(smtConnector);
+  }
 
   private Map<String, String> smtProperties(
       String smtTopic, String smtConnector, String behaviorOnNull) {
@@ -35,35 +53,26 @@ public class SmtIT extends ConnectClusterBaseIT {
   @ParameterizedTest
   @CsvSource({"DEFAULT, 20", "IGNORE, 10"})
   void testIfSmtReturningNullsIngestDataCorrectly(String behaviorOnNull, int expectedRecordNumber) {
-    String smtTopic = TestUtils.randomTableName();
-    String smtConnector = String.format("%s_connector", smtTopic);
+    // given
+    connectCluster.configureConnector(
+        smtConnector, smtProperties(smtTopic, smtConnector, behaviorOnNull));
+    waitForConnectorRunning(smtConnector);
 
-    try {
-      // given
-      connectCluster.kafka().createTopic(smtTopic);
-      connectCluster.configureConnector(
-          smtConnector, smtProperties(smtTopic, smtConnector, behaviorOnNull));
-      waitForConnectorRunning(smtConnector);
+    // when
+    Stream.iterate(0, UnaryOperator.identity())
+        .limit(10)
+        .flatMap(v -> Stream.of("{}", "{\"message\":\"value\"}"))
+        .forEach(message -> connectCluster.kafka().produce(smtTopic, message));
 
-      // when
-      Stream.iterate(0, UnaryOperator.identity())
-          .limit(10)
-          .flatMap(v -> Stream.of("{}", "{\"message\":\"value\"}"))
-          .forEach(message -> connectCluster.kafka().produce(smtTopic, message));
-
-      // then
-      await()
-          .timeout(Duration.ofSeconds(60))
-          .untilAsserted(
-              () -> {
-                assertThat(fakeStreamingClientHandler.ingestedRows()).hasSize(expectedRecordNumber);
-                assertThat(fakeStreamingClientHandler.getLatestCommittedOffsetTokensPerChannel())
-                    .hasSize(1)
-                    .containsValue("19");
-              });
-    } finally {
-      connectCluster.kafka().deleteTopic(smtTopic);
-      connectCluster.deleteConnector(smtConnector);
-    }
+    // then
+    await()
+        .timeout(Duration.ofSeconds(60))
+        .untilAsserted(
+            () -> {
+              assertThat(fakeStreamingClientHandler.ingestedRows()).hasSize(expectedRecordNumber);
+              assertThat(fakeStreamingClientHandler.getLatestCommittedOffsetTokensPerChannel())
+                  .hasSize(1)
+                  .containsValue("19");
+            });
   }
 }
