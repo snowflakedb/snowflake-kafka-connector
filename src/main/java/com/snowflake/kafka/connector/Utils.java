58ba07f5ab283b517a882b0859cdbedafaf0c76f
Robert Sawicki <robert.sawicki@snowflake.com>
2024-10-16 13:07:42 +0200

    Release 2.5.0

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -66,7 +66,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "2.4.1";
+  public static final String VERSION = "2.5.0";
 
   // connector parameter list
   public static final String NAME = "name";
