ade8fd0a8557b0caec3edb965a6c3cb49af3cc8a
Wojciech Trefon <wojciech.trefon@snowflake.com>
2024-10-16 12:00:40 +0200

    Refactor test

--- src/test/java/com/snowflake/kafka/connector/streaming/iceberg/IcebergIngestionIT.java
+++ src/test/java/com/snowflake/kafka/connector/streaming/iceberg/IcebergIngestionIT.java
@@ -20,6 +20,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaAndValue;
@@ -28,7 +29,9 @@ import org.apache.kafka.connect.sink.SinkRecord;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class IcebergIngestionIT extends BaseIcebergIT {
 
@@ -41,7 +44,7 @@ public class IcebergIngestionIT extends BaseIcebergIT {
 
   private final String simpleRecordJson = "{\"simple\": \"extra field\"}";
 
-  private final String primitiveJson =
+  private static final String primitiveJson =
       "{\n"
           + "  \"id_int8\": 0,\n"
           + "  \"id_int16\": 42,\n"
@@ -53,7 +56,7 @@ public class IcebergIngestionIT extends BaseIcebergIT {
           + "  \"approval\": true\n"
           + "}";
 
-  private final String primitiveJsonWithSchema =
+  private static final String primitiveJsonWithSchema =
       "{\n"
           + "  \"schema\": {\n"
           + "    \"type\": \"struct\",\n"
@@ -133,16 +136,18 @@ public class IcebergIngestionIT extends BaseIcebergIT {
     dropIcebergTable(tableName);
   }
 
-  @Test
+  @ParameterizedTest()
+  @MethodSource("prepareData")
   @Disabled
-  void shouldInsertJsonRecordWithoutSchema() throws Exception {
+  void shouldEvolveSchemaAndInsertRecords(
+      String description, String message, DescribeTableRow[] expectedSchema, boolean withSchema)
+      throws Exception {
     // start off with just one column
     List<DescribeTableRow> rows = describeTable(tableName);
     assertThat(rows.size()).isEqualTo(1);
     assertThat(rows.get(0).getColumn()).isEqualTo(Utils.TABLE_COLUMN_METADATA);
 
-    // send first record which should be discarded but schema evolution should kick in
-    SinkRecord record = createKafkaRecord(primitiveJson, 0, false);
+    SinkRecord record = createKafkaRecord(message, 0, withSchema);
     service.insert(Collections.singletonList(record));
     TestUtils.assertWithRetry(
         () -> service.getOffset(new TopicPartition(topic, PARTITION)) == -1, 20, 5);
@@ -154,16 +159,8 @@ public class IcebergIngestionIT extends BaseIcebergIT {
         rows.stream()
             .filter(r -> !r.getColumn().equals(Utils.TABLE_COLUMN_METADATA))
             .collect(Collectors.toList());
-    assertThat(rows)
-        .containsExactlyInAnyOrder(
-            new DescribeTableRow("ID_INT8", "NUMBER(19,0)"),
-            new DescribeTableRow("ID_INT16", "NUMBER(19,0)"),
-            new DescribeTableRow("ID_INT32", "NUMBER(19,0)"),
-            new DescribeTableRow("ID_INT64", "NUMBER(19,0)"),
-            new DescribeTableRow("DESCRIPTION", "VARCHAR(16777216)"),
-            new DescribeTableRow("RATING_FLOAT32", "FLOAT"),
-            new DescribeTableRow("RATING_FLOAT64", "FLOAT"),
-            new DescribeTableRow("APPROVAL", "BOOLEAN"));
+
+    assertThat(rows).containsExactlyInAnyOrder(expectedSchema);
 
     // resend and store same record without any issues now
     service.insert(Collections.singletonList(record));
@@ -171,7 +168,7 @@ public class IcebergIngestionIT extends BaseIcebergIT {
         () -> service.getOffset(new TopicPartition(topic, PARTITION)) == 1, 20, 5);
 
     // and another record with same schema
-    service.insert(Collections.singletonList(createKafkaRecord(primitiveJson, 1, false)));
+    service.insert(Collections.singletonList(createKafkaRecord(message, 1, withSchema)));
     TestUtils.assertWithRetry(
         () -> service.getOffset(new TopicPartition(topic, PARTITION)) == 2, 20, 5);
 
@@ -188,58 +185,36 @@ public class IcebergIngestionIT extends BaseIcebergIT {
         () -> service.getOffset(new TopicPartition(topic, PARTITION)) == 3, 20, 5);
   }
 
-  @Test
-  @Disabled
-  void shouldInsertJsonRecordWithSchema() throws Exception {
-    // start off with just one column
-    List<DescribeTableRow> rows = describeTable(tableName);
-    assertThat(rows.size()).isEqualTo(1);
-    assertThat(rows.get(0).getColumn()).isEqualTo(Utils.TABLE_COLUMN_METADATA);
-
-    SinkRecord record = createKafkaRecord(primitiveJsonWithSchema, 0, true);
-    service.insert(Collections.singletonList(record));
-    TestUtils.assertWithRetry(
-        () -> service.getOffset(new TopicPartition(topic, PARTITION)) == -1, 20, 5);
-    rows = describeTable(tableName);
-    assertThat(rows.size()).isEqualTo(9);
-
-    // don't check metadata column schema, we have different tests for that
-    rows =
-        rows.stream()
-            .filter(r -> !r.getColumn().equals(Utils.TABLE_COLUMN_METADATA))
-            .collect(Collectors.toList());
-    assertThat(rows)
-        .containsExactlyInAnyOrder(
-            new DescribeTableRow("ID_INT8", "NUMBER(10,0)"),
-            new DescribeTableRow("ID_INT16", "NUMBER(10,0)"),
-            new DescribeTableRow("ID_INT32", "NUMBER(10,0)"),
-            new DescribeTableRow("ID_INT64", "NUMBER(19,0)"),
-            new DescribeTableRow("DESCRIPTION", "VARCHAR(16777216)"),
-            new DescribeTableRow("RATING_FLOAT32", "FLOAT"),
-            new DescribeTableRow("RATING_FLOAT64", "FLOAT"),
-            new DescribeTableRow("APPROVAL", "BOOLEAN"));
-
-    // resend and store same record without any issues now
-    service.insert(Collections.singletonList(record));
-    TestUtils.assertWithRetry(
-        () -> service.getOffset(new TopicPartition(topic, PARTITION)) == 1, 20, 5);
-
-    // and another record with same schema
-    service.insert(Collections.singletonList(createKafkaRecord(primitiveJsonWithSchema, 1, true)));
-    TestUtils.assertWithRetry(
-        () -> service.getOffset(new TopicPartition(topic, PARTITION)) == 2, 20, 5);
-
-    // and another record with extra field - schema evolves again
-    service.insert(Collections.singletonList(createKafkaRecord(simpleRecordJson, 2, false)));
-
-    rows = describeTable(tableName);
-    assertThat(rows.size()).isEqualTo(10);
-    assertThat(rows).contains(new DescribeTableRow("SIMPLE", "VARCHAR(16777216)"));
-
-    // reinsert record with extra field
-    service.insert(Collections.singletonList(createKafkaRecord(simpleRecordJson, 2, false)));
-    TestUtils.assertWithRetry(
-        () -> service.getOffset(new TopicPartition(topic, PARTITION)) == 3, 20, 5);
+  private static Stream<Arguments> prepareData() {
+    return Stream.of(
+        Arguments.of(
+            "Primitive JSON with schema",
+            primitiveJsonWithSchema,
+            new DescribeTableRow[] {
+              new DescribeTableRow("ID_INT8", "NUMBER(10,0)"),
+              new DescribeTableRow("ID_INT16", "NUMBER(10,0)"),
+              new DescribeTableRow("ID_INT32", "NUMBER(10,0)"),
+              new DescribeTableRow("ID_INT64", "NUMBER(19,0)"),
+              new DescribeTableRow("DESCRIPTION", "VARCHAR(16777216)"),
+              new DescribeTableRow("RATING_FLOAT32", "FLOAT"),
+              new DescribeTableRow("RATING_FLOAT64", "FLOAT"),
+              new DescribeTableRow("APPROVAL", "BOOLEAN")
+            },
+            true),
+        Arguments.of(
+            "Primitive JSON without schema",
+            primitiveJson,
+            new DescribeTableRow[] {
+              new DescribeTableRow("ID_INT8", "NUMBER(19,0)"),
+              new DescribeTableRow("ID_INT16", "NUMBER(19,0)"),
+              new DescribeTableRow("ID_INT32", "NUMBER(19,0)"),
+              new DescribeTableRow("ID_INT64", "NUMBER(19,0)"),
+              new DescribeTableRow("DESCRIPTION", "VARCHAR(16777216)"),
+              new DescribeTableRow("RATING_FLOAT32", "FLOAT"),
+              new DescribeTableRow("RATING_FLOAT64", "FLOAT"),
+              new DescribeTableRow("APPROVAL", "BOOLEAN")
+            },
+            false));
   }
 
   private SinkRecord createKafkaRecord(String jsonString, int offset, boolean withSchema) {
