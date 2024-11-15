f775818f83f808ac43185bfc93190cd1b241ab5f
Bartłomiej Ząbek <bartlomiej.zabek@snowflake.com>
2024-11-15 09:00:20 +0100

    add iceberg-common license

--- scripts/process_licenses.py
+++ scripts/process_licenses.py
@@ -93,7 +93,8 @@ ADDITIONAL_LICENSES_MAP = {
     "org.xerial.snappy:snappy-java": APACHE_LICENSE,
     "org.yaml:snakeyaml": APACHE_LICENSE,
     "org.apache.iceberg:iceberg-api": APACHE_LICENSE,
-    "org.apache.iceberg:iceberg-core": APACHE_LICENSE
+    "org.apache.iceberg:iceberg-core": APACHE_LICENSE,
+    "org.apache.iceberg:iceberg-common": APACHE_LICENSE
 }
 
 
