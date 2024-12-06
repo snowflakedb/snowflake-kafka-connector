768d9fa2b91294bdfacd79cc0eba5328c556c423
Michał Bobowski <145468486+sfc-gh-mbobowski@users.noreply.github.com>
2024-12-06 15:16:17 +0100

    NO-SNOW Release 3.0.0 (#1022)

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -66,7 +66,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "2.5.0";
+  public static final String VERSION = "3.0.0";
 
   // connector parameter list
   public static final String NAME = "name";
