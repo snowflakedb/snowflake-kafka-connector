0cdb1aa46ca64c2f84ba533a2b687a74bd5c25da
Adrian Kowalczyk <adrian.kowalczyk@snowflake.com>
2024-12-20 13:53:20 +0100

    POC works

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -83,6 +83,7 @@ public class Utils {
   public static final String SF_OAUTH_CLIENT_ID = "snowflake.oauth.client.id";
   public static final String SF_OAUTH_CLIENT_SECRET = "snowflake.oauth.client.secret";
   public static final String SF_OAUTH_REFRESH_TOKEN = "snowflake.oauth.refresh.token";
+  public static final String SF_OAUTH_TOKEN_ENDPOINT = "snowflake.oauth.token.endpoint";
 
   // authenticator type
   public static final String SNOWFLAKE_JWT = "snowflake_jwt";
@@ -716,7 +717,7 @@ public class Utils {
    * @return OAuth access token
    */
   public static String getSnowflakeOAuthAccessToken(
-      SnowflakeURL url, String clientId, String clientSecret, String refreshToken) {
+          com.snowflake.kafka.connector.internal.URL url, String clientId, String clientSecret, String refreshToken) {
     return getSnowflakeOAuthToken(
         url,
         clientId,
@@ -743,7 +744,7 @@ public class Utils {
    */
   // TODO: SNOW-895296 Integrate OAuth utils with streaming ingest SDK
   public static String getSnowflakeOAuthToken(
-      SnowflakeURL url,
+      com.snowflake.kafka.connector.internal.URL url,
       String clientId,
       String clientSecret,
       String credential,
@@ -778,7 +779,7 @@ public class Utils {
         new StringEntity(payloadString, ContentType.APPLICATION_FORM_URLENCODED);
 
     HttpPost post =
-        buildOAuthHttpPostRequest(url, OAuthConstants.TOKEN_REQUEST_ENDPOINT, headers, entity);
+        buildOAuthHttpPostRequest(url, url.path(), headers, entity);
 
     // Request access token
     CloseableHttpClient client = HttpClientBuilder.create().build();
@@ -812,12 +813,12 @@ public class Utils {
    * @return HttpPost request for OAuth
    */
   public static HttpPost buildOAuthHttpPostRequest(
-      SnowflakeURL url, String path, Map<String, String> headers, StringEntity entity) {
+          com.snowflake.kafka.connector.internal.URL url, String path, Map<String, String> headers, StringEntity entity) {
     // Build post request
     URI uri;
     try {
       uri =
-          new URIBuilder().setHost(url.toString()).setScheme(url.getScheme()).setPath(path).build();
+          new URIBuilder().setHost(url.hostWithPort()).setScheme(url.getScheme()).setPath(path).build();
     } catch (URISyntaxException e) {
       throw SnowflakeErrors.ERROR_1004.getException(e);
     }
