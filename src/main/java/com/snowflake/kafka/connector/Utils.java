5b818824aaa59439fc99a65c15c1ba41852aee56
Adrian Kowalczyk <adrian.kowalczyk@snowflake.com>
2025-01-16 13:40:39 +0100

    Release 3.1.0

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -65,7 +65,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "3.0.0";
+  public static final String VERSION = "3.1.0";
 
   // connector parameter list
   public static final String NAME = "name";
