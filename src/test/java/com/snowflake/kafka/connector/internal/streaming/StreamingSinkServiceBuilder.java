389161b16bd3f651fe9364d8b060fc5b8cdb2a07
Michał Bobowski <145468486+sfc-gh-mbobowski@users.noreply.github.com>
2025-04-24 09:48:41 +0200

    CONN-10472 Introducing StreamingSinkServiceBuilder (#1091)

---
+++ src/test/java/com/snowflake/kafka/connector/internal/streaming/StreamingSinkServiceBuilder.java
@@ -0,0 +1,62 @@
+package com.snowflake.kafka.connector.internal.streaming;
+
+import com.snowflake.kafka.connector.dlq.InMemoryKafkaRecordErrorReporter;
+import com.snowflake.kafka.connector.dlq.KafkaRecordErrorReporter;
+import com.snowflake.kafka.connector.internal.SnowflakeConnectionService;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.connect.sink.SinkTaskContext;
+
+public class StreamingSinkServiceBuilder {
+
+  private final SnowflakeConnectionService conn;
+  private final Map<String, String> connectorConfig;
+
+  private KafkaRecordErrorReporter errorReporter = new InMemoryKafkaRecordErrorReporter();
+  private SinkTaskContext sinkTaskContext = new InMemorySinkTaskContext(Collections.emptySet());
+  private boolean enableCustomJMXMonitoring = false;
+  private Map<String, String> topicToTableMap = new HashMap<>();
+
+  public static StreamingSinkServiceBuilder builder(
+      SnowflakeConnectionService conn, Map<String, String> connectorConfig) {
+    return new StreamingSinkServiceBuilder(conn, connectorConfig);
+  }
+
+  public SnowflakeSinkServiceV2 build() {
+    return new SnowflakeSinkServiceV2(
+        conn,
+        connectorConfig,
+        errorReporter,
+        sinkTaskContext,
+        enableCustomJMXMonitoring,
+        topicToTableMap);
+  }
+
+  private StreamingSinkServiceBuilder(
+      SnowflakeConnectionService conn, Map<String, String> connectorConfig) {
+    this.conn = conn;
+    this.connectorConfig = connectorConfig;
+  }
+
+  public StreamingSinkServiceBuilder withErrorReporter(
+      InMemoryKafkaRecordErrorReporter errorReporter) {
+    this.errorReporter = errorReporter;
+    return this;
+  }
+
+  public StreamingSinkServiceBuilder withSinkTaskContext(SinkTaskContext sinkTaskContext) {
+    this.sinkTaskContext = sinkTaskContext;
+    return this;
+  }
+
+  public StreamingSinkServiceBuilder withEnableCustomJMXMetrics(boolean enableCustomJMXMetrics) {
+    this.enableCustomJMXMonitoring = enableCustomJMXMetrics;
+    return this;
+  }
+
+  public StreamingSinkServiceBuilder withTopicToTableMap(Map<String, String> topic2TableMap) {
+    topicToTableMap = topic2TableMap;
+    return this;
+  }
+}
