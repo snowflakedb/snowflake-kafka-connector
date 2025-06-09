f253c3bbfb38b00cc43a9277b34156e9dd12bd18
Michal Bobowski <michal.bobowski@snowflake.com>
2025-06-09 17:05:31 +0200

    CONN-10463 TestSnowpipeStreamingNullableValuesAfterSmt -> AWS only

--- test/test_suites.py
+++ test/test_suites.py
@@ -274,7 +274,7 @@ def create_end_to_end_test_suites(driver, nameSalt, schemaRegistryAddress, testS
                     ),
                     run_in_confluent=True,
                     run_in_apache=True,
-                    cloud_platform=CloudPlatform.ALL,
+                    cloud_platform=CloudPlatform.AWS,
                 ),
             ),
             (
