bbe99dff07987bafcbff4293be652177d1923442
Michal Bobowski <michal.bobowski@snowflake.com>
2025-06-12 16:43:50 +0200

    Release 4.0.0

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -65,7 +65,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "3.2.0";
+  public static final String VERSION = "4.0.0";
 
   // connector parameter list
   public static final String NAME = "name";
