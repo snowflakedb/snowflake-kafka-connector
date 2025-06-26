b006c6a081a3795e02cbca06b16694109f530812
Michał Bobowski <145468486+sfc-gh-mbobowski@users.noreply.github.com>
2025-06-26 14:53:05 +0200

    FLOW-4165 Bump confluent packages to 7.9.2 / Release 3.2.2 (#1123)

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -65,7 +65,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "3.2.1";
+  public static final String VERSION = "3.2.2";
 
   // connector parameter list
   public static final String NAME = "name";
