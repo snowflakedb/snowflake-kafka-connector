a9e47090383927c4311eec678f4694103cc93e0a
Wojciech Trefon <wojciech.trefon@snowflake.com>
2025-10-17 09:44:34 +0200

    Release v3.3.1

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -65,7 +65,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "3.3.0";
+  public static final String VERSION = "3.3.1";
 
   // connector parameter list
   public static final String NAME = "name";
