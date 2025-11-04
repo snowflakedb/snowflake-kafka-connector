7f630ee1ccc7c56744b23900a8b288f9c79744b4
Lukasz Kucharski <lukasz.kucharski@snowflake.com>
2025-11-04 21:03:44 +0100

    Fix formatting

--- src/test/java/com/snowflake/kafka/connector/internal/SnowflakeConnectionServiceV1Test.java
+++ src/test/java/com/snowflake/kafka/connector/internal/SnowflakeConnectionServiceV1Test.java
@@ -2,18 +2,11 @@ package com.snowflake.kafka.connector.internal;
 
 import static com.snowflake.kafka.connector.internal.SnowflakeConnectionServiceV1.FormattingUtils.formatName;
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.snowflake.kafka.connector.internal.streaming.ChannelMigrateOffsetTokenResponseDTO;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class SnowflakeConnectionServiceV1Test {
-
 
   @ParameterizedTest
   @CsvSource({"role, ROLE", "Role, ROLE", "\"role\", role", "\"rOle\", rOle"})
