0661489ae81ce1cf4c7744b2e3a409983632e460
Dariusz Seweryn <dariusz.seweryn@snowflake.com>
2025-11-05 20:17:42 +0100

    Release v3.4.0 (#1208)

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -67,7 +67,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "3.3.1";
+  public static final String VERSION = "3.4.0";
 
   // connector parameter list
   public static final String NAME = "name";
