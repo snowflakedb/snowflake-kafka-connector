bbb939251757c8456dcca36d149fe56b1d3bf65e
Lukasz Kucharski <lukasz.kucharski@snowflake.com>
2025-11-18 13:20:11 +0100

    Fixes after 1st round by Michał Bobowski

--- src/main/java/com/snowflake/kafka/connector/internal/streaming/validation/RowSchemaProvider.java
+++
@@ -1,7 +0,0 @@
-package com.snowflake.kafka.connector.internal.streaming.validation;
-
-import java.util.Map;
-
-public interface RowSchemaProvider {
-  RowSchema getRowSchema(String tableName, Map<String, String> connectorConfig);
-}
--- test/test_verify.py
+++ test/test_verify.py
@@ -64,9 +64,9 @@ class KafkaTest:
         self.httpHeader = {'Content-type': 'application/json', 'Accept': 'application/json'}
 
         self.SEND_INTERVAL = 0.01  # send a record every 10 ms
-        self.VERIFY_INTERVAL = 10  # verify every 60 secs
-        self.MAX_RETRY = 60  # max wait time 10 mins
-        self.MAX_FLUSH_BUFFER_SIZE = 5000  # flush buffer when 10000 data was in the queue
+        self.VERIFY_INTERVAL = 10  # verify every 10 secs
+        self.MAX_RETRY = 60  # max wait time 1 min
+        self.MAX_FLUSH_BUFFER_SIZE = 5000  # flush buffer when 5000 data was in the queue
 
         self.kafkaConnectAddress = kafkaConnectAddress
         self.schemaRegistryAddress = schemaRegistryAddress
