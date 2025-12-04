306b17884e2867959d1a197f0b7198b796f1bc18
Lukasz Kucharski <lukasz.kucharski@snowflake.com>
2025-12-04 18:10:38 +0100

    NO-SNOW Release 4.0.0-rc4

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -44,7 +44,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "4.0.0-rc3";
+  public static final String VERSION = "4.0.0-rc4";
 
   // constants strings
   private static final String KAFKA_OBJECT_PREFIX = "SNOWFLAKE_KAFKA_CONNECTOR";
