de483be5e7a6183d6c667c8e3aa48dae70a9fd16
Lukasz Kucharski <lukasz.kucharski@snowflake.com>
2026-01-13 15:53:09 +0100

    NO-SNOW - Release 3.5.2

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -67,7 +67,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "3.5.1";
+  public static final String VERSION = "3.5.2";
 
   // connector parameter list
   public static final String NAME = "name";
