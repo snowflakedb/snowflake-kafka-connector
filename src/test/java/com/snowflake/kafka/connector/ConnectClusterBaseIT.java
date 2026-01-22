fb31ab0a4e1f4f215e6736cf8ec08025046529c4
Lukasz Kucharski <lukasz.kucharski@snowflake.com>
2026-01-22 12:00:53 +0100

    SNOW-3011964 Add test scenario confirming timestamp conversion quirks + refactor test

--- src/test/java/com/snowflake/kafka/connector/ConnectClusterBaseIT.java
+++ src/test/java/com/snowflake/kafka/connector/ConnectClusterBaseIT.java
@@ -1,31 +1,57 @@
 package com.snowflake.kafka.connector;
 
+import static com.snowflake.kafka.connector.Constants.KafkaConnectorConfigParams.SNOWFLAKE_TOPICS2TABLE_MAP;
 import static org.awaitility.Awaitility.await;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.snowflake.kafka.connector.Constants.KafkaConnectorConfigParams;
+import com.snowflake.kafka.connector.internal.SnowflakeConnectionService;
+import com.snowflake.kafka.connector.internal.SnowflakeConnectionServiceFactory;
 import com.snowflake.kafka.connector.internal.TestUtils;
 import com.snowflake.kafka.connector.internal.streaming.FakeIngestClientSupplier;
 import com.snowflake.kafka.connector.internal.streaming.FakeSnowflakeStreamingIngestClient;
+import com.snowflake.kafka.connector.internal.streaming.v2.StreamingClientManager;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.testutil.MockSchemaRegistry;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.connect.json.JsonConverter;
 import org.apache.kafka.connect.runtime.ConnectorConfig;
 import org.apache.kafka.connect.sink.SinkConnector;
 import org.apache.kafka.connect.storage.StringConverter;
 import org.apache.kafka.connect.util.clusters.EmbeddedConnectCluster;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 
 /** Base class for integration tests using an embedded Kafka Connect cluster. */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class ConnectClusterBaseIT {
 
-  protected EmbeddedConnectCluster connectCluster;
-  protected final FakeIngestClientSupplier fakeClientSupplier = new FakeIngestClientSupplier();
-
+  static final String MOCK_SCHEMA_REGISTRY_URL = "mock://test-schema-registry";
+  static final String SCHEMA_REGISTRY_SCOPE = "test-schema-registry";
+  static final int PARTITION_COUNT = 1;
+  static final int RECORD_COUNT = 100;
+  static final int TOPIC_COUNT = 2;
   static final Integer TASK_NUMBER = 1;
+
+  protected final FakeIngestClientSupplier fakeClientSupplier = new FakeIngestClientSupplier();
+  protected final ObjectMapper objectMapper = new ObjectMapper();
+
+  protected String tableName;
+  protected String connectorName;
+  protected String topic0;
+  protected String topic1;
+  protected EmbeddedConnectCluster connectCluster;
+  protected SnowflakeConnectionService snowflake;
 
   @BeforeAll
   public void beforeAll() {
@@ -49,6 +75,34 @@ public abstract class ConnectClusterBaseIT {
       connectCluster.stop();
       connectCluster = null;
     }
+  }
+
+  @BeforeEach
+  void before() {
+
+    tableName = TestUtils.randomTableName();
+    connectorName = String.format("%s_connector", tableName);
+    topic0 = tableName + "0";
+    topic1 = tableName + "1";
+    connectCluster.kafka().createTopic(topic0, PARTITION_COUNT);
+    connectCluster.kafka().createTopic(topic1, PARTITION_COUNT);
+    snowflake =
+        SnowflakeConnectionServiceFactory.builder()
+            .setProperties(TestUtils.transformProfileFileToConnectorConfiguration(false))
+            .noCaching()
+            .build();
+
+    StreamingClientManager.resetIngestClientSupplier();
+  }
+
+  @AfterEach
+  void after() {
+    connectCluster.kafka().deleteTopic(topic0);
+    connectCluster.kafka().deleteTopic(topic1);
+    connectCluster.deleteConnector(connectorName);
+    StreamingClientManager.resetIngestClientSupplier();
+    TestUtils.dropTable(tableName);
+    MockSchemaRegistry.dropScope(SCHEMA_REGISTRY_SCOPE);
   }
 
   protected FakeSnowflakeStreamingIngestClient getOpenedFakeIngestClient(String connectorName) {
@@ -113,6 +167,44 @@ public abstract class ConnectClusterBaseIT {
     } catch (InterruptedException e) {
       throw new IllegalStateException("The connector is not running");
     }
+  }
+
+  protected KafkaProducer<String, Object> createAvroProducer() {
+    final Properties props = new Properties();
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, connectCluster.kafka().bootstrapServers());
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+    props.put("schema.registry.url", MOCK_SCHEMA_REGISTRY_URL);
+    return new KafkaProducer<>(props, new StringSerializer(), createAvroSerializer());
+  }
+
+  protected KafkaAvroSerializer createAvroSerializer() {
+    final SchemaRegistryClient schemaRegistryClient =
+        MockSchemaRegistry.getClientForScope(SCHEMA_REGISTRY_SCOPE);
+    final KafkaAvroSerializer serializer = new KafkaAvroSerializer(schemaRegistryClient);
+    serializer.configure(Map.of("schema.registry.url", MOCK_SCHEMA_REGISTRY_URL), false);
+    return serializer;
+  }
+
+  protected Map<String, String> createConnectorConfig() {
+    final String topics = topic0 + "," + topic1;
+    final String topicsToTableMap = topic0 + ":" + tableName + "," + topic1 + ":" + tableName;
+
+    final Map<String, String> config = defaultProperties(topics, connectorName);
+    config.put(SNOWFLAKE_TOPICS2TABLE_MAP, topicsToTableMap);
+    config.put(ConnectorConfig.KEY_CONVERTER_CLASS_CONFIG, StringConverter.class.getName());
+    config.put("value.converter.schemas.enable", "false");
+    config.put("errors.tolerance", "none");
+    config.put("errors.log.enable", "true");
+    config.put("errors.deadletterqueue.topic.name", "DLQ_TOPIC");
+    config.put("errors.deadletterqueue.topic.replication.factor", "1");
+    config.put("jmx", "true");
+    return config;
+  }
+
+  void sendTombstoneRecords(final String topic) {
+    // Send null tombstone
+    connectCluster.kafka().produce(topic, null);
   }
 
   private FakeSnowflakeStreamingIngestClient getFakeSnowflakeStreamingIngestClient(
