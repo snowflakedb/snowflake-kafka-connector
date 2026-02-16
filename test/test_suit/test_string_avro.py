65c5e75ecc57b9bfc68047583f7ecbe7075ee8ca
Seb Kurella <seb.kurella@snowflake.com>
2026-02-16 20:55:06 +0000

    move KafkaTest to lib and improve

--- test/test_suit/test_string_avro.py
+++ test/test_suit/test_string_avro.py
@@ -15,7 +15,7 @@ class TestStringAvro(BaseE2eTest):
         return self.fileName + ".json"
 
     def send(self):
-        avroBytes = open(self.driver.TEST_DATA_FOLDER + "twitter.avro", "rb").read()
+        avroBytes = (self.driver.TEST_DATA_FOLDER / "twitter.avro").read_bytes()
         value = []
         # only append 50 times because the file have two records
         for e in range(50):
