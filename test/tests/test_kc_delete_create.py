9ff30f5007479fa22f3f0b076ed0681e35c0c289
Seb Kurella <seb.kurella@snowflake.com>
2026-02-19 19:00:15 +0000

    port test_kc_delete_create

---
+++ test/tests/test_kc_delete_create.py
@@ -0,0 +1,45 @@
+import json
+from time import sleep
+
+FILE_NAME = "test_kc_delete_create"
+CONFIG_FILE = f"{FILE_NAME}.json"
+RECORD_COUNT = 1000
+SLEEP_TIME = 10
+
+
+def _send_batch(driver, topic, record_count):
+    values = [
+        json.dumps({"column1": str(i)}).encode("utf-8") for i in range(record_count)
+    ]
+    driver.sendBytesData(topic, values, [], 0)
+    sleep(2)
+
+
+def test_kc_delete_create(
+    driver, name_salt, create_connector, snowflake_table, wait_for_rows
+):
+    topic = snowflake_table(
+        FILE_NAME,
+        f"CREATE OR REPLACE TABLE {FILE_NAME}{name_salt} "
+        f"(record_metadata variant, column1 varchar)",
+    )
+
+    connector_name = f"{FILE_NAME}{name_salt}"
+    driver.createTopics(topic, partitionNum=1, replicationNum=1)
+
+    create_connector(CONFIG_FILE)
+    driver.startConnectorWaitTime()
+
+    # -- Send 1/2, delete, create, send 2/2 --
+    _send_batch(driver, topic, RECORD_COUNT)
+
+    driver.deleteConnector(connector_name)
+    sleep(SLEEP_TIME)
+
+    driver.createConnector(CONFIG_FILE, name_salt)
+    sleep(SLEEP_TIME)
+
+    _send_batch(driver, topic, RECORD_COUNT)
+
+    # -- Verify --
+    wait_for_rows(topic, RECORD_COUNT * 2)
