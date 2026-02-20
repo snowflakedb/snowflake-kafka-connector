bb5302b1583b5baf08f8a37ca71a72cd13dd6f6f
Seb Kurella <seb.kurella@snowflake.com>
2026-02-20 21:47:56 +0000

    remove ExposingInternalsTopicPartitionChannel

--- src/main/java/com/snowflake/kafka/connector/internal/streaming/channel/TopicPartitionChannel.java
+++ src/main/java/com/snowflake/kafka/connector/internal/streaming/channel/TopicPartitionChannel.java
@@ -1,11 +1,14 @@
 package com.snowflake.kafka.connector.internal.streaming.channel;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.snowflake.ingest.streaming.SFException;
-import java.util.Map;
+import com.snowflake.kafka.connector.internal.streaming.telemetry.SnowflakeTelemetryChannelStatus;
+import dev.failsafe.Fallback;
 import java.util.concurrent.CompletableFuture;
+import java.util.Map;
 import org.apache.kafka.connect.sink.SinkRecord;
 
-public interface TopicPartitionChannel extends ExposingInternalsTopicPartitionChannel {
+public interface TopicPartitionChannel {
   long NO_OFFSET_TOKEN_REGISTERED_IN_SNOWFLAKE = -1L;
 
   /**
@@ -73,4 +76,33 @@ public interface TopicPartitionChannel extends ExposingInternalsTopicPartitionCh
   default CompletableFuture<Void> waitForLastProcessedRecordCommitted() {
     return CompletableFuture.completedFuture(null);
   }
+
+  @VisibleForTesting
+  SnowflakeTelemetryChannelStatus getSnowflakeTelemetryChannelStatus();
+
+  /**
+   * Fetches the offset token from Snowflake.
+   *
+   * <p>It uses <a href="https://github.com/failsafe-lib/failsafe">Failsafe library </a> which
+   * implements retries, fallbacks and circuit breaker.
+   *
+   * <p>Here is how Failsafe is implemented.
+   *
+   * <p>Fetches offsetToken from Snowflake (Streaming API)
+   *
+   * <p>If it returns a valid offset number, that number is returned back to caller.
+   *
+   * <p>If {@link com.snowflake.ingest.streaming.SFException} is thrown, we will retry for max 3
+   * times. (Including the original try)
+   *
+   * <p>Upon reaching the limit of maxRetries, we will {@link Fallback} to opening a channel and
+   * fetching offsetToken again.
+   *
+   * <p>Please note, upon executing fallback, we might throw an exception too. However, in that case
+   * we will not retry.
+   *
+   * @return long offset token present in snowflake for this channel/partition.
+   */
+  @VisibleForTesting
+  long fetchOffsetTokenWithRetry();
 }
