11c337c3ea33c8d2e768022e7ff70359a365158a
Seb Kurella <seb.kurella@snowflake.com>
2026-03-27 15:08:55 +0100

    bump version for release 4.0.0-rc9 (#1391)

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -42,7 +42,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "4.0.0-rc8";
+  public static final String VERSION = "4.0.0-rc9";
 
   // task id
   public static final String TASK_ID = "task_id";
