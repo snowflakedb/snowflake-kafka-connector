7ba38b67baa5aa565967169b9e77ab66cf6d470a
Seb Kurella <seb.kurella@snowflake.com>
2026-06-25 17:19:20 +0200

    bump version to 4.0.2 (#1503)
    Bump project version from 4.0.1 to 4.0.2 in `pom.xml`,
    `pom_confluent.xml`, and the `Utils.VERSION` constant.

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -42,7 +42,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "4.0.1";
+  public static final String VERSION = "4.0.2";
 
   // task id
   public static final String TASK_ID = "task_id";
